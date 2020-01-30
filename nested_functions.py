f049f047178b6ec029677be2c7baf5155c873cf5
Erjan <erjan@users.noreply.github.com>
2020-01-30 22:00:27 +0600

    commented out the descr

--- nested_functions.py
+++ nested_functions.py
@@ -1,8 +1,8 @@
-Good morning! Here's your coding interview problem for today.
-This problem was asked by Jane Street.
-cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns 
-the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
-Given this implementation of cons:
+# Good morning! Here's your coding interview problem for today.
+# This problem was asked by Jane Street.
+# cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns 
+# the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+# Given this implementation of cons:
 
 #sit for 30 mins and did it myself
 
