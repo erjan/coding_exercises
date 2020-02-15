9d9b38be5e6eb209d43e28625b34960f8432dcc7
Erjan <erjan@users.noreply.github.com>
2020-02-15 16:23:47 +0600

    improved the code with back diagonal

--- find_diagonal_diff_square_matrix.py
+++ find_diagonal_diff_square_matrix.py
@@ -16,17 +16,25 @@ def generate_matrix():
         return matrix
 
 def print_matrix(m):
-        for row in range(len(m)):
-                print()
-                print(m[row])
+    print('generated matrix:' )
+    for row in range(len(m)):            
+            print(m[row])
+            print()
 
 
+def rev_matrix(m):
+    for row in m:
+        row.reverse()
+    return m
 
+
+      
 def get_back_diagonal(m):
         sum_diag = 0 
         for row in range(len(m)):
                 count = len(m)-1 - row
                 #print(count)
+                
                 for j in range(len(m[row])):
                         #print(m[row][count], end = ' ')
                         sum_diag += m[row][count]
@@ -44,8 +52,16 @@ def get_diagonal(m):
                                 sum_diag += m[row][j]
         return sum_diag
                         
-                        
+
+def print_diagonal(m):
+        for row in range(len(m)):
+                for j in range(len(m[row])):
+                        if row == j:
+                                print(m[row][j],end = ' ')
+        print()
+                  
 if __name__ == '__main__':
+        '''
         matrix = []
         n = int(input())
         for row in range(n):
@@ -55,17 +71,28 @@ if __name__ == '__main__':
         d1 = get_diagonal(matrix)
         d2 = get_back_diagonal(matrix)
         print( abs(d1-d2))
-
         #print_matrix(matrix)
         '''
-        m = generate_matrix()
-        print_matrix(m)
+        matrix = generate_matrix()
+        print_matrix(matrix)
         print()
         print()
+
+        d1 = get_diagonal(matrix)
+        d2 = get_diagonal(rev_matrix(matrix))
+        print('diagonal:')
+        print_diagonal(matrix)
+        print('back diagonal:')
+
+        print_diagonal(rev_matrix(matrix))
+        
+        print('the abs sum of diagonals: %d' %abs(d1-d2))
+        
+        '''
         d1 = print_diagonal(m)
         d2 = print_back_diagonal(m)
         print(d1)
         print(d2)
         print( abs(d1-d2))
         '''
-        
+       
