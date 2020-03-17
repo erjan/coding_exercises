72d5b3f958d0a534225eaaebbace506a575a745d
Erjan <erjan@users.noreply.github.com>
2020-03-18 00:01:20 +0600

    explanation is what matters here..
    i made this file to explain myself how it happens recursively!

---
+++ reverse_linked_list_recursion.py
@@ -0,0 +1,46 @@
+class LinkedNode: 
+    # Constructor to initialize the node object 
+    def __init__(self, data): 
+        self.val = data 
+        self.next = None
+
+m1 = LinkedNode(4)
+m2 = LinkedNode(5)
+m3 = LinkedNode(7)
+m4 = LinkedNode(8)
+m1.next = m2
+m2.next = m3
+m3.next = m4
+
+def print_list(l):
+      
+    while l:
+        if l.next == None:
+            print(l.val)
+        else:
+            print(l.val, end = ' -> ')
+        l = l.next
+        
+def reverse (item, tail = None):
+    print()
+    print('beginning.... %d' % item.val)
+    print('tail before:')
+    print_list(tail)
+    tmp = item.next
+    item.next = tail
+    print('tail after:')
+    print_list(item.next)
+    if tmp is None:
+        print('end of list!')
+        print_list(item)
+        return item
+    else:
+        print()
+        print()
+        print('recursive..')
+        return reverse(tmp, item)
+        
+        
+   print_list(m1)
+   
+   d = reverse(m1, None)
