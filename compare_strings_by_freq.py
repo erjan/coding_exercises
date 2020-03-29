8bb411c2e278caad669dd858c72e49aa57264fb8
Erjan <erjan@users.noreply.github.com>
2020-03-29 14:19:37 +0600

    found a bug

--- compare_strings_by_freq.py
+++ compare_strings_by_freq.py
@@ -24,3 +24,99 @@ class Solution:
             answer.append(c)
         print(answer)
         return answer
+    
+    
+    
+    # i found my bug!
+    #i turned queries into dictionary and this removed 1 value from actual query list.
+    
+   #the value removed was a repeating key in the dictionary: 'ba'
+
+def f( queries, words):
+        
+    def helper( s):
+            
+        min_s = min(s)
+        return s.count(min_s)
+            
+    queries_dict = dict()
+    print('len of queries dict % d ' %len(queries_dict))
+    for q in queries:
+        queries_dict[q] = helper(q)
+
+
+    print(queries)
+    print(list(queries_dict.keys()))
+
+
+    words_dict = dict()
+    for w in words:
+        words_dict[w] = helper(w)
+    
+    answer = []
+    print('len of words dict %d ' % len(words_dict))
+
+    q_values = list(queries_dict.values())
+    w_values = list(words_dict.values())
+
+    print('queries freqs: %s ' % q_values)
+    print('words freqs: %s ' % w_values)
+    print('len of queries: %d ' % len(q_values))
+
+    print('len of words: %d ' % len(w_values))
+    '''
+    for q in q_values:
+        #print()
+        #print('-----------------------------------------------' )
+        #time.sleep(0.75)
+        c = 0
+        for w in w_values:
+            #print('comparing q %d vs word %d ' %(q, w))
+            if q < w:
+                #time.sleep(0.6)
+                #print('found, so +1: %d < %d ' %(q,w))
+                c+=1
+            else:
+                #print('!!!! q > w')
+                #print('else %d > %d' %(q,w))
+                
+            #else:
+                #c = len(w_values
+            
+        
+            
+        #l = list(filter(lambda x : q < x, w_values))
+        #if c== 0:
+         #   print('found none ')
+          #  c+= len(q_values)
+        #print('after: c %d, appending %d' % (c,c))
+        #answer.append(c)
+        
+    print('len of answer %d '  % len(answer))
+    print('answer')
+    print(answer)
+    
+    expected = [6,5,0,6,11,11,11,8,11,0,6,6]
+    print('expected')
+    print(expected)
+
+    print()
+    print('query freqs %s' % q_values)
+    print('word freqs %s' % w_values)
+      '''    
+    return 0
+
+'''                         
+q=["aabbabbb","abbbabaa","aabbbabaa","aabba","abb","a","ba","aa","ba","baabbbaaaa","babaa","bbbbabaa"]
+w = ["b","aaaba","aaaabba","aa","aabaabab","aabbaaabbb","ababb","bbb","aabbbabb","aab","bbaaababba","baaaaa"]
+
+q = ["bba","abaaaaaa","aaaaaa","bbabbabaab","aba","aa","baab","bbbbbb","aab","bbabbaabb"]
+w = ["aaabbb","aab","babbab","babbbb","b","bbbbbbbbab","a","bbbbbbbbbb","baaabbaab","aa"]
+'''
+  
+q = ["aabbabbb","abbbabaa","aabbbabaa","aabba","abb","a","ba","aa","ba","baabbbaaaa","babaa","bbbbabaa"]
+w = ["b","aaaba","aaaabba","aa","aabaabab","aabbaaabbb","ababb","bbb","aabbbabb","aab","bbaaababba","baaaaa"]
+
+f(q,w)
+    
+    
