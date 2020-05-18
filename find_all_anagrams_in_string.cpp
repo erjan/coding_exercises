0fc22a092fab88d1c52d4d1dea97b0980caf2ba4
Erjan <erjan@users.noreply.github.com>
2020-05-19 00:06:21 +0600

    Create find_all_anagrams_in_string.cpp

---
+++ find_all_anagrams_in_string.cpp
@@ -0,0 +1,41 @@
+/*
+Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+
+Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+
+The order of output does not matter.
+*/
+class Solution {
+    
+public:
+    vector<int> findAnagrams(string s, string p) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        vector<int> ans;
+        vector<int> hash(26,0);
+        vector<int> phash(26,0);
+        int window = p.size();
+        int len = s.size();
+        if(len<window)
+            return ans;
+        int left = 0,right = 0;
+        while(right<window)
+        {
+            phash[p[right]-'a'] +=1;
+            hash[s[right++]-'a'] +=1;
+        }
+        right -=1;
+        while(right<len)
+        {
+            if(phash == hash)
+                ans.push_back(left);
+            right+=1;
+            if(right!=len)
+                hash[s[right]-'a'] +=1;
+            hash[s[left]-'a'] -=1;
+            left+=1;
+        }
+        return ans;
+    }
+};
