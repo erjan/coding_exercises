0eebc19718d35e2a1ade268273300e0a6eb4cc1f
Erjan <erjan@users.noreply.github.com>
2020-06-12 14:29:51 +0600

    Create insert_delete_getrandom.py

---
+++ insert_delete_getrandom.py
@@ -0,0 +1,55 @@
+'''
+Design a data structure that supports all following operations in average O(1) time.
+
+insert(val): Inserts an item val to the set if not already present.
+remove(val): Removes an item val from the set if present.
+getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+'''
+
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.ds = set()
+        
+        
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val not in self.ds:
+            self.ds.add(val)
+            return True
+        return False
+        
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.ds:
+            self.ds.remove(val)
+            return True
+        return False
+        
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        len_set = len(self.ds)
+        num = random.randint(0, len_set-1)
+        l = list(self.ds)
+        return l[num]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
