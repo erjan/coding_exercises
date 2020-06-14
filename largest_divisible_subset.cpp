cff725a03df614d08c6711ecbee052d03e9bcba5
Erjan <erjan@users.noreply.github.com>
2020-06-15 00:17:39 +0600

    Create largest_divisible_subset.cpp

---
+++ largest_divisible_subset.cpp
@@ -0,0 +1,47 @@
+'''Given a set of distinct positive integers, 
+find the largest subset such that every pair (Si, Sj) of elements in this subset satisfies:
+
+Si % Sj = 0 or Sj % Si = 0.
+
+If there are multiple solutions, return any subset is fine.
+'''
+
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> ans;
+        if(n==0)
+            return ans;
+        
+        sort(nums.begin(),nums.end());
+        int max = 1;
+        vector<int> dp(n+1,1);
+        for(int i=1;i<n;++i)
+        {
+            for(int j=0;j<i;++j)
+            {
+                if(nums[i]%nums[j]==0 && 1+dp[j]>dp[i])
+                {
+                    dp[i] = 1+dp[j];
+                    if(max<dp[i])
+                        max = dp[i];
+                }
+            }
+        }
+        //Now push the answer LIS
+        int prev = -1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(dp[i]==max &&  (prev%nums[i]==0 || prev==-1))
+            {
+                ans.push_back(nums[i]);
+                max -=1;
+                prev = nums[i];
+            }
+        }
+        return ans;
+    }
+};
