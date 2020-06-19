57926cc2549ea28d7aad53e03fb905e0bc113ace
Erjan <erjan@users.noreply.github.com>
2020-06-19 16:39:57 +0600

    fixed bugs

--- binary_search.py
+++ binary_search.py
@@ -1,8 +1,10 @@
 import random
 
 def binary_search(val, arr):
+    if arr == None or len(arr) == 0:
+        return -1
     low = 0
-    high = len(arr)
+    high = len(arr)-1
     mid = 0
     print('looking for value %d' % val)
     while low <=high:
@@ -17,9 +19,19 @@ def binary_search(val, arr):
             high = mid-1
         elif arr[mid] < val:
             low = mid+1
+    print('no value found! -1')
+    return -1
 
 l = random.sample( range(14000),390)
-
+l = [1,2,5,7,11,15,20]
 l = sorted(l)
 
-binary_search(l[125],l)
+#test cases:
+binary_search(-10, l)
+binary_search(0, l)
+binary_search(1, [])
+binary_search(1, None)
+binary_search(0, [1,2,3])
+binary_search(10,[1,2,3,20,30,40,50])
+binary_search(30, l)
+
