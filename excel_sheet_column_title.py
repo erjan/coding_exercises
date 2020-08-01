b02de4f322788929434fd72018fe03ec93265a4d
Erjan <erjan@users.noreply.github.com>
2020-08-02 03:05:15 +0600

    Update excel_sheet_column_title.py

--- excel_sheet_column_title.py
+++ excel_sheet_column_title.py
@@ -4,7 +4,7 @@ Given a positive integer, return its corresponding column title as appear in an 
 
 
 from string import ascii_uppercase as abc
-
+#imperative solution
 class Solution:
     def convertToTitle(self, n: int) -> str:
         res = ''
@@ -13,3 +13,11 @@ class Solution:
             res = abc[n%26]+res
             n = n //26
         return res
+
+#recursive solution
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        if n >0:
+            n = n-1
+            return self.convertToTitle(n//26) + abc[n%26]
+        return ''
