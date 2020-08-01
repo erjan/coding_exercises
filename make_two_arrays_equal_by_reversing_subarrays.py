c1ba4401696ad4f7fb083f568f0e147fbc93db30
Erjan <erjan@users.noreply.github.com>
2020-08-01 21:26:53 +0600

    looked up other solutions.

--- make_two_arrays_equal_by_reversing_subarrays.py
+++ make_two_arrays_equal_by_reversing_subarrays.py
@@ -11,4 +11,46 @@ Return True if you can make arr equal to target, or False otherwise.
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
         return sorted(target) == sorted(arr)
+    
+    
+#SOLUTION I LOOKED UP using dictionaries or collections counter
+
+#this is just counting how many times each digit appears - another condition of equality
+def f( target, arr):
+    d1={}
+    d2={}
+    for i in target:
+        if i in d1:
+            d1[i]+=1
+        else:
+            d1[i]=1
+    print('d1 after filling')
+    print(d1)
+    
+    for i in arr:
+        if i in d2:
+            d2[i]+=1
+        else:
+            d2[i]=1
+
+    print('d2 after filing')
+    print(d2)
+    return (d1==d2)
+
+target = [1,2,3,4]
+arr = [2,4,1,3]
+
+
+f(target,arr)
+
+#same with counter
+from collections import Counter
+
+def f( target, arr):
+    c_target = Counter(target)
+    c_arr = Counter(arr)
+    
+    result = c_target == c_arr
+    print(result)
+
         
