213308af1931f6d909cb1edce12ea60790919f58
Erjan <erjan@users.noreply.github.com>
2020-08-01 20:28:46 +0600

    Update pascal_triangle.py

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -23,3 +23,25 @@ class Solution:
             temp = helper(self,prev)
             res.append(temp)
         return res
+    
+#ONE FUNCTION
+  
+def generate(numRows):
+    res = []
+    res.append([1]) 
+    if numRows == 0:
+        return []
+        
+    for i in range(1,numRows):
+        prev = res[i-1]
+     
+        t = []
+        for i in range(len(prev)-1):
+            temp = prev[i] + prev[i+1]
+            t.append(temp)
+        t.insert(0,1)
+        t.insert( len(t),1)
+
+        res.append(t)
+    print(res)
+    return res
