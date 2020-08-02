5b5b557d7716a263fc0a8e77a533e084f2d2d843
Erjan <erjan@users.noreply.github.com>
2020-08-02 12:46:55 +0600

    Create count_good_triplets.py

---
+++ count_good_triplets.py
@@ -0,0 +1,26 @@
+'''
+Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+
+A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+
+0 <= i < j < k < arr.length
+|arr[i] - arr[j]| <= a
+|arr[j] - arr[k]| <= b
+|arr[i] - arr[k]| <= c
+Where |x| denotes the absolute value of x.
+
+Return the number of good triplets.
+'''
+
+
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        num_triplets = 0
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                for k in range(j+1, len(arr)):
+                    if abs(arr[i] - arr[j]) <=a:
+                        if abs(arr[j] - arr[k]) <=b:
+                            if abs(arr[i]-arr[k]) <=c:
+                                num_triplets +=1
+        return num_triplets
