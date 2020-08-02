4b9255902d5b7ec32d8e8378a2e281f454399e89
Erjan <erjan@users.noreply.github.com>
2020-08-02 13:46:05 +0600

    Create count_largest_group.py

---
+++ count_largest_group.py
@@ -0,0 +1,28 @@
+'''
+Given an integer n. Each number from 1 to n is grouped according to the sum of its digits. 
+
+Return how many groups have the largest size.
+'''
+
+
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        list_sums = dict.fromkeys([i for i in range(1,37)])
+        for i in list_sums.keys():
+            list_sums[i] = 0
+
+        for i in range(1,n+1):
+            num = str(i)
+            num = list(num)
+            num = list(map(lambda s: int(s),num))
+            temp_sum = sum(num)
+            if temp_sum in list_sums.keys():
+                list_sums[temp_sum]+=1
+
+        max_size = max(list_sums.values())
+        result= 0
+        for k in list_sums.keys():
+            if list_sums[k] == max_size:
+                result+=1
+        print(result)
+        return result
