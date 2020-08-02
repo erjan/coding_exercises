d9a9249574c106359a55fbc2e1d4930d4b319666
Erjan <erjan@users.noreply.github.com>
2020-08-02 13:50:20 +0600

    looked up a simplified solution

--- count_largest_group.py
+++ count_largest_group.py
@@ -26,3 +26,26 @@ class Solution:
                 result+=1
         print(result)
         return result
+
+    
+#a bit simplified solution
+
+def f(n):
+    list_sums = dict.fromkeys([i for i in range(1,37)])
+    for i in list_sums.keys():
+        list_sums[i] = 0
+
+    for i in range(1,n+1):
+        num = str(i)
+        num = [int(c) for c in num]
+        if sum(num) in list_sums.keys():
+            list_sums[sum(num)]+=1
+
+    max_size = max(list_sums.values())
+    result= 0
+    for k in list_sums.keys():
+        if list_sums[k] == max_size:
+            result+=1
+    return result
+
+f(2)
