e616765eb6ab6b9c89cb01bb8972fd1b49e94f27
Erjan <erjan@users.noreply.github.com>
2020-08-02 12:15:47 +0600

    included randomized search solution

--- n_repeated_elements_in_size_2n_array.py
+++ n_repeated_elements_in_size_2n_array.py
@@ -13,3 +13,13 @@ class Solution:
                 d[A[i]] = 1
             else:
                 return A[i]
+
+            
+#randomized solution
+
+class Solution:
+    def repeatedNTimes(self, A):
+        while 1:
+            s = random.sample(A, 2)
+            if s[0] == s[1]:
+                return s[0]
