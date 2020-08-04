15943b68ad87f8dbd87513f6254318f7b873ef0f
Erjan <erjan@users.noreply.github.com>
2020-08-04 18:32:12 +0600

    Create subdomain_visit_count.py

---
+++ subdomain_visit_count.py
@@ -0,0 +1,38 @@
+'''
+A website domain like "discuss.leetcode.com" consists of various 
+subdomains. At the top level, we have "com", at the next level, we have "leetcode.com", and at the
+lowest level, "discuss.leetcode.com". When we visit a domain like "discuss.leetcode.com", we will also 
+visit the parent domains "leetcode.com" and "com" implicitly.
+
+Now, call a "count-paired domain" to be a count 
+(representing the number of visits this domain received), followed by a space, followed by 
+the address. An example of a count-paired domain might be "9001 discuss.leetcode.com".
+
+We are given a list cpdomains of count-paired domains. We would like a list of 
+count-paired domains, (in the same format as the input, and in any order), that explicitly 
+counts the number of visits to each subdomain.
+'''
+
+
+
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        d = dict()
+        
+        for domain in cpdomains:
+            count, domain = domain.split(' ')
+            count = int(count)
+            domain = domain.split('.')
+
+            for i in range(len(domain)):
+                subd = '.'.join(domain[i:])
+                if subd not in d:
+                    d[subd] = count
+
+                else:
+                    d[subd] += count
+                    
+        res = []     
+        for item in d.keys():
+            res.append( str(d[item]) + ' ' + item)
+        return res
