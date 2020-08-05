db2ca5c79f0f0b0630b2577489d87e9d8d80525a
Erjan <erjan@users.noreply.github.com>
2020-08-06 01:46:02 +0600

    uploaded solution from knowledge center

--- add_and_search_word.py
+++ add_and_search_word.py
@@ -65,3 +65,43 @@ param1 = obj.search('b..')
 
 
 print(param1)
+
+
+#solution from knowledge center
+
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.children = [None]*26
+        self.endword = False
+        
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        curr = self
+        for c in word:
+            if curr.children[ ord(c) - ord('a')] == None:
+                curr.children[ ord(c) - ord('a')] = WordDictionary()
+            curr = curr.children[ ord(c) - ord('a')]
+        curr.endword = True
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        """
+        curr = self
+        for i in range(len(word)):
+            c = word[i]
+            if c == '.':
+                for ch in curr.children:
+                    if ch!= None and ch.search(word[i+1:]):return True
+                return False
+            if curr.children[ord(c) - ord('a')] == None:return False
+            curr = curr.children[ord(c) - ord('a')]
+        return curr != None and curr.endword
