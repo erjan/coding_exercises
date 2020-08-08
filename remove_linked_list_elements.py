16829360446df49db1b1b955247838d331504eb1
Erjan <erjan@users.noreply.github.com>
2020-08-09 02:44:23 +0600

    Create remove_linked_list_elements.py

---
+++ remove_linked_list_elements.py
@@ -0,0 +1,48 @@
+'''
+Remove all elements from a linked list of integers that have value val.
+
+Example:
+
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+'''
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head is None:
+            return None
+
+        cur = head
+        prev = None
+        
+        if cur.next is None:
+            if cur.val == val:
+                return prev
+            else:
+                return cur
+         
+        while cur:
+            if cur.val == val:
+                if prev is None:
+                    
+                    cur = cur.next
+                    head = cur #this was the catch!!!!!! - looked up in knowledge center channel
+                    
+                else:
+                    cur = cur.next
+                    prev.next = cur
+
+            else:
+                prev = cur
+                cur = cur.next
+
+
+        return head
+    
