fffb52833c937c17a212605de95d29e1286e2494
Erjan <erjan@users.noreply.github.com>
2020-08-08 18:23:10 +0600

    Create linked_list_cycle.py

---
+++ linked_list_cycle.py
@@ -0,0 +1,28 @@
+'''
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents 
+the position (0-indexed) in the linked list where tail connects to. If pos is -1, then 
+there is no cycle in the linked list.
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            
+            slow= slow.next
+            fast = fast.next.next
+        return True
