a8580a194e5d9364f64b3224aab4e7c6f1e42dbb
Erjan <erjan@users.noreply.github.com>
2022-01-21 04:23:51 +0600

    Update range_sum_bst.py

--- range_sum_bst.py
+++ range_sum_bst.py
@@ -25,3 +25,50 @@ class Solution:
         return self.total_s
             
             
+#============================================
+#brute force
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
+        if root is None:
+            return 0
+        
+        elif low <= root.val <= high:
+            return self.rangeSumBST(root.left,low,high) + root.val + self.rangeSumBST(root.right, low,high)
+        
+        else:
+            return self.rangeSumBST(root.left,low,high) + self.rangeSumBST(root.right,low,high)
+        
+        
+#optimized:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
+        if root is None:
+            return 0
+        
+        elif low <= root.val <= high:
+            return self.rangeSumBST(root.left,low,high) + root.val + self.rangeSumBST(root.right, low,high)
+        
+        elif root.val < low:
+            return self.rangeSumBST(root.right,low,high)
+        elif root.val > high:
+            return self.rangeSumBST(root.left,low,high)
+            
+            
+            
+            
