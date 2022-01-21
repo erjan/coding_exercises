7a1f570376d3fe7e25eaecf449a3d0500f56bde7
Erjan <erjan@users.noreply.github.com>
2022-01-21 20:49:56 +0600

    Update merge_two_binary_trees.py

--- merge_two_binary_trees.py
+++ merge_two_binary_trees.py
@@ -26,4 +26,43 @@ class Solution:
         return helper(r1,r2)
         
       
-    
+#iterative solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+
+        q = deque([(root1,root2)])
+
+        while q:
+            cur1, cur2 = q.pop()
+
+            if cur1.left and cur2.left:
+                q.append((cur1.left, cur2.left))
+
+            elif not cur1.left:
+                cur1.left = cur2.left
+
+            if cur1.right and cur2.right:
+                q.append((cur1.right, cur2.right))
+
+            elif not cur1.right:
+                cur1.right = cur2.right
+
+            cur1.val = cur1.val + cur2.val
+
+        return root1
+       
+        
