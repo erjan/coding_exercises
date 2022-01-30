fe19ee18aff3722fc4e3cbe263320e8a3f4d064d
Erjan <erjan@users.noreply.github.com>
2022-01-30 12:30:02 +0600

    Create remove_element.py

---
+++ remove_element.py
@@ -0,0 +1,15 @@
+'''
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+'''
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k+=1
