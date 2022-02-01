042380202b27d5810158dc4869184e9dd81df686
Erjan <erjan@users.noreply.github.com>
2022-02-01 13:12:08 +0600

    adding another eloquent solution i looked up

--- path_sum.py
+++ path_sum.py
@@ -29,3 +29,23 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         
         return self.check(root, 0, targetSum)        
+# much better solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        if root is None:
+            return False
+        elif not root.left and not root.right and root.val== targetSum:
+            return True
+        targetSum = targetSum - root.val
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+        
+        
