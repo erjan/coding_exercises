5f8a3ae115b16f2cbb6fc1186bdc3a52158eaaeb
Erjan <erjan@users.noreply.github.com>
2022-02-13 02:03:13 +0600

    Update and rename sotring_the_sentence.py to sorting_the_sentence.py

--- sorting_the_sentence.py
+++ sorting_the_sentence.py
@@ -28,3 +28,15 @@ class Solution:
 
        
         return res
+    
+    #better solution from submissions
+    
+    class Solution:
+    def sortSentence(self, s: str) -> str:
+        
+        x = s.split()
+        dic = {}
+        for i in x :
+            dic[i[-1]] = i[:-1]
+        return ' '.join([dic[j] for j in sorted(dic)])
+
