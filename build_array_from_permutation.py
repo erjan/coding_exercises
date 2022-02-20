d35b0415d12e6322319d5c4d786b25645ff5a316
Erjan <erjan@users.noreply.github.com>
2022-02-20 12:12:44 +0600

    Create build_array_from_permutation.py

---
+++ build_array_from_permutation.py
@@ -0,0 +1,15 @@
+'''
+Given a zero-based permutation nums (0-indexed), build an array ans of the same 
+length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+'''
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = list()
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
+            
