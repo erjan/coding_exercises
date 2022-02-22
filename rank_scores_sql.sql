d79be39548a0d57280fe7c066af474562de2225c
Erjan <erjan@users.noreply.github.com>
2022-02-22 16:09:42 +0600

    Create rank_scores_sql.sql

---
+++ rank_scores_sql.sql
@@ -0,0 +1,15 @@
+'''
+Write an SQL query to rank the scores. The ranking should be calculated according to the following rules:
+
+The scores should be ranked from the highest to the lowest.
+If there is a tie between two scores, both should have the same ranking.
+After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+Return the result table ordered by score in descending order.
+
+The query result format is in the following example.
+'''
+
+/* Write your T-SQL query statement below */
+select score, dense_rank() over(order by score desc) as rank 
+from scores
+
