70e09836422ff7af023cce98d8c6c2c097a1f678
Erjan <erjan@users.noreply.github.com>
2022-02-23 07:47:53 +0600

    uploaded 2nd simpler solution

--- department_highest_salary.sql
+++ department_highest_salary.sql
@@ -17,3 +17,9 @@ SELECT
     FROM department d inner join employee e on e.departmentId = d.id)tmp
     
     where rn <=1
+
+
+SELECT dep.Name as Department, emp.Name as Employee, emp.Salary 
+from Department dep, Employee emp 
+where emp.DepartmentId=dep.Id 
+and emp.Salary=(Select max(Salary) from Employee e2 where e2.DepartmentId=dep.Id)
