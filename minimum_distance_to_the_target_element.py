5762c4cca4469e7156b855601c216b7d97811ea0
Erjan <erjan@users.noreply.github.com>
2022-03-16 02:01:42 +0600

    Update minimum_distance_to_the_target_element.py

--- minimum_distance_to_the_target_element.py
+++ minimum_distance_to_the_target_element.py
@@ -27,4 +27,14 @@ class Solution:
                     min_res = cur
         return min_res
                 
+#another simpler solution                
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        
+        l = list()
+        for i in range(len(nums)):
+            
+            if nums[i] == target:
                 
+                l.append(abs(i - start))
+        return min(l)
