02f25f046ac40c3c345e595567aebdd12b5b3d6b
Erjan <erjan@users.noreply.github.com>
2022-03-16 18:43:03 +0600

    Update minimum_difference_btw_highest_and_lowest_of_k_scores.py

--- minimum_difference_btw_highest_and_lowest_of_k_scores.py
+++ minimum_difference_btw_highest_and_lowest_of_k_scores.py
@@ -25,3 +25,25 @@ class Solution:
                 total = cur
         print(total)
         return total
+
+    
+
+
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        if len(nums) == 1:
+            return 0
+
+        nums.sort()
+        res = math.inf
+        
+        for i in range(len(nums)-k+1): #this is important!!! -k+1 , +1 is needed! otherwise wrong answer!
+
+            sliding_window = nums[i: (i+k)]
+            cur = max(sliding_window) - min(sliding_window)
+            if cur < res:
+                #print('found new low: ', cur)
+
+                res = cur
+        print(res)
+        return res    
