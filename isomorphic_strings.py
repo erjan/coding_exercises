73d6b6b5124e16c0162fa3f1da8ce8fb6aa22784
Erjan <erjan@users.noreply.github.com>
2022-03-16 22:51:29 +0600

    Create isomorphic_strings.py

---
+++ isomorphic_strings.py
@@ -0,0 +1,35 @@
+'''
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+'''
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s2t = dict()
+        t2s = dict()
+
+        for i in range(len(s)):
+
+            if s[i] not in s2t:
+
+                s2t[s[i]] = t[i]
+            elif s[i] in s2t:
+                if s2t[s[i]] != t[i]:
+                    print('bad')
+                    return False
+
+            if t[i] not in t2s:
+                t2s[t[i]] = s[i]
+
+            elif t[i] in t2s:
+                if t2s[t[i]] != s[i]:
+                    print('bad')
+                    return False
+        print('good')
+        return True
