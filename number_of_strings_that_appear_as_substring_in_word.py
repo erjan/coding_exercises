aaf15e4330aded54fd861623e02baf8ae3215496
Erjan <erjan@users.noreply.github.com>
2022-03-19 00:20:59 +0600

    Create number_of_strings_that_appear_as_substring_in_word.py

---
+++ number_of_strings_that_appear_as_substring_in_word.py
@@ -0,0 +1,16 @@
+'''
+Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+
+A substring is a contiguous sequence of characters within a string.
+'''
+
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        p = patterns
+        
+        c = 0
+        for w in p:
+            if w in word:
+                c += 1
+        print(c)
+        return c
