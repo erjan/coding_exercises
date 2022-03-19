ff6a8de34fb44cde7f8d017366d9a59913ec3eb1
Erjan <erjan@users.noreply.github.com>
2022-03-20 04:12:18 +0600

    Update divide_array_into_equal_pairs.py

--- divide_array_into_equal_pairs.py
+++ divide_array_into_equal_pairs.py
@@ -21,3 +21,22 @@ class Solution:
                 return False 
             i+=2
         return True
+    
+    
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        
+        d = dict()
+        
+        for n in nums:
+            
+            if n not in d:
+                d[n] = 1
+            else:
+                d[n] +=1                
+        d = d.values()        
+        for v in d:
+            if v%2 != 0:
+                return False
+        return True
+                
