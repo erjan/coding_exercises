12c3811cb1b617992f34ce6d4c186cefad4aaa40
Erjan <erjan@users.noreply.github.com>
2022-03-20 03:41:32 +0600

    Create kth_largest_element_in_array.py

---
+++ kth_largest_element_in_array.py
@@ -0,0 +1,21 @@
+'''
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+'''
+#my own solution - O(nlogn)
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        res = nums[-k]
+        return res
+      
+      
+#many other follow ups possible?!
+
+
+
+# O(k+(n-k)lgk) time, min-heap    
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+            return heapq.nlargest(k, nums)[-1]
