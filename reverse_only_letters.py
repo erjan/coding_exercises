575154e0b0de152f4ce96f4f815949ffd230ba0e
Erjan <erjan@users.noreply.github.com>
2022-03-21 04:25:51 +0600

    Update reverse_only_letters.py

--- reverse_only_letters.py
+++ reverse_only_letters.py
@@ -27,3 +27,38 @@ class Solution:
         print('result')
         print(res)
         return res
+
+    
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+
+        size = len(s)
+        
+        # convert into array of characters
+        array = [*s]
+        
+        # two pointers, initialized with boundary index
+        left, right = 0, size-1
+        
+        #  swap english letter with two-pointers
+        while left < right:
+            
+            # keep moving until we meet english letter on left hand side
+            if not array[left].isalpha():
+                left += 1
+                continue
+            
+            # keep moving until we meet english letter on right hand side
+            if not array[right].isalpha():
+                right -= 1
+                continue
+            
+            # swap english letters
+            array[left], array[right] = array[right], array[left]
+            
+            # update index of two pointer
+            left, right = left+1, right-1
+        
+        
+        # convert to string and output
+        return "".join(array)    
