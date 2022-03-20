81756c68b275168ef0462d2ea715674b1ffe1d51
Erjan <erjan@users.noreply.github.com>
2022-03-21 04:54:56 +0600

    Create n_ary_tree_postorder_traversal.py

---
+++ n_ary_tree_postorder_traversal.py
@@ -0,0 +1,31 @@
+'''
+Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+'''
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        output = list()
+        stack = list()
+
+        if root is None:
+            return output
+
+        stack.append(root)
+        while stack:
+            cur = stack.pop()
+            output.append(cur.val)
+            for c in cur.children:
+                stack.append(c)
+        print(output)
+        output = output[::-1]
+        return output
