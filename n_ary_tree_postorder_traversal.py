2ca9c59443292ce2c8f133456b59d78777312ede
Erjan <erjan@users.noreply.github.com>
2022-03-21 05:29:32 +0600

    Update n_ary_tree_postorder_traversal.py

--- n_ary_tree_postorder_traversal.py
+++ n_ary_tree_postorder_traversal.py
@@ -43,3 +43,23 @@ class Solution:
         out = []
         rec(root)
         return out
+    
+    
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        
+        postorder_traversal = []
+        stack = [[root, False]]
+        
+        while stack:
+            curr, visited = stack.pop()
+            if visited:
+                postorder_traversal.append(curr.val)
+            else:
+                stack.append([curr, True])
+                children = len(curr.children)
+                for i in range(children - 1, -1, -1):
+                    stack.append([curr.children[i], False])
+        return postorder_traversal    
