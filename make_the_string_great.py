b977f2d33c0918643966ab7dee9e80e0ca471b81
Erjan <erjan@users.noreply.github.com>
2022-03-21 21:43:32 +0600

    Update make_the_string_great.py

--- make_the_string_great.py
+++ make_the_string_great.py
@@ -35,3 +35,16 @@ class Solution:
         res = ''.join(res)
         print(res)
         return res
+       
+class Solution:
+    def makeGood(self, s: str) -> str:
+        i = 0
+        while i < len(s)-1:
+            if s[i] == s[i+1]:
+                i += 1
+            elif s[i].lower() == s[i+1] or s[i].upper() == s[i+1]:
+                s = s[:i] + s[i+2:]
+                i = 0
+            else:
+                i += 1        
+        return s       
