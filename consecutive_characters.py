743f1fe9b7982e6d52a73fa04a13afc1e13188a5
Erjan <erjan@users.noreply.github.com>
2022-03-21 08:00:32 +0600

    Update consecutive_characters.py

--- consecutive_characters.py
+++ consecutive_characters.py
@@ -18,3 +18,35 @@ class Solution:
                 maxi = len(s[i])
         print(maxi)
         return maxi
+
+       
+#more classical solution
+
+class Solution:
+    def maxPower(self, s: str) -> int:
+        
+        # the minimum value for consecutive is 1
+        local_max, global_max = 1, 1
+        
+        # dummy char for initialization
+        prev = '#'
+        for char in s:
+            
+            if char == prev:
+                
+                # keeps consecutive, update local max
+                local_max += 1
+                
+                # update global max length with latest one
+                global_max = max( global_max, local_max )
+                
+            else:
+                
+                # lastest consective chars stops, reset local max
+                local_max = 1
+            
+                # update previous char as current char for next iteration
+                prev = char
+        
+        
+        return global_max
