8663a74d614c9b140bdd51df697a853fcf141b0a
Erjan <erjan@users.noreply.github.com>
2022-03-21 06:47:58 +0600

    Update long_pressed_name.py

--- long_pressed_name.py
+++ long_pressed_name.py
@@ -20,3 +20,26 @@ class Solution:
                 return False
 
         return True
+# 2 pointer solution
+
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        n=len(name)
+        m=len(typed)
+        
+        if m<n:
+            return False
+        i=j=0
+        while(True):
+            print(i,j)
+            if i==n and j==m:
+                return True
+            
+            if i<n and j<m and name[i]==typed[j]:
+                i+=1
+                j+=1
+            elif j>0 and j<m and typed[j-1]==typed[j]:
+
+                j+=1
+            else:
+                return False
