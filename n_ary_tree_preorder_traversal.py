42839c8030bb6812bb1a8ffe33f5a0336b6e78de
Erjan <erjan@users.noreply.github.com>
2022-03-22 02:06:20 +0600

    Update n_ary_tree_preorder_traversal.py

--- n_ary_tree_preorder_traversal.py
+++ n_ary_tree_preorder_traversal.py
@@ -16,3 +16,88 @@ class Solution:
         out = []
         rec(root)
         return out
+
+    
+
+pakilamak's avatar
+pakilamak
+474
+Last Edit: August 11, 2020 11:59 PM
+
+2.8K VIEWS
+
+Recursive Solution: Runtime: 36 ms, faster than 97.16%
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        
+        output =[]
+        
+        # perform dfs on the root and get the output stack
+        self.dfs(root, output)
+        
+        # return the output of all the nodes.
+        return output
+    
+    def dfs(self, root, output):
+        
+        # If root is none return 
+        if root is None:
+            return
+        
+        # for preorder we first add the root val
+        output.append(root.val)
+        
+        # Then add all the children to the output
+        for child in root.children:
+            self.dfs(child, output)
+       
+Iterative Solution- Runtime: 40 ms, faster than 91.86%
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        
+        stack = [root]
+        output = []
+        
+        # Till there is element in stack the loop runs.
+        while stack:
+            
+            #pop the last element from the stack and store it into temp.
+            temp = stack.pop()
+            
+            # append. the value of temp to output
+            output.append(temp.val)
+            
+            #add the children of the temp into the stack in reverse order.
+            # children of 1 = [3,2,4], if not reveresed then 4 will be popped out first from the stack.
+            # if reversed then stack = [4,2,3]. Here 3 will pop out first.
+            # This continues till the stack is empty.
+            stack.extend(temp.children[::-1])
+        
+        #return the output
+        return output    
