454f3fdc88fbd5b6fc2113c6bd30105c08809c2c
Erjan <erjan@users.noreply.github.com>
2022-03-21 06:06:37 +0600

    Update valid_boomerang.py

--- valid_boomerang.py
+++ valid_boomerang.py
@@ -5,17 +5,9 @@ A boomerang is a set of three points that are all distinct and not in a straight
 
 '''
 
+
 class Solution:
     def isBoomerang(self, points: List[List[int]]) -> bool:
+        (x0, y0), (x1, y1), (x2, y2) = points
         
-        point1 = points[0]
-        point2 = points[1]
-        point3 = points[2]
-
-        temp1 = point1[0] * (  point2[1] - point3[1]    )
-        temp2 = point2[0] * (  point3[1] - point2[1]    )
-        temp3 = point3[0] * (  point1[1] - point2[1]    )
-
-        if temp1 + temp2 + temp3 !=0:
-            return True
-        return False
+        return (y2 - y1) * (x0 - x1) != (x2 - x1) * (y0 - y1)
