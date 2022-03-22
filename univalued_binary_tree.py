bdcd4a328a755a89afaa7b062476438240c5dccd
Erjan <erjan@users.noreply.github.com>
2022-03-22 15:59:57 +0600

    Update univalued_binary_tree.py

--- univalued_binary_tree.py
+++ univalued_binary_tree.py
@@ -30,3 +30,16 @@ class Solution:
         result = check(root,main)
         return result
         
+#from discussions        
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        
+        if root.left and root.val != root.left.val:
+            return False
+            
+        if root.right and root.val != root.right.val:
+            return False
+        
+        return self.isUnivalTree(root.left) and self.isUnivalTree(root.right)        
