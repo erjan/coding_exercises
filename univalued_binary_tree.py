1d174772ef6ad858b39b14c1e682ceeb8c38ebd8
Erjan <erjan@users.noreply.github.com>
2022-03-22 15:58:05 +0600

    Create univalued_binary_tree.py

---
+++ univalued_binary_tree.py
@@ -0,0 +1,32 @@
+'''
+A binary tree is uni-valued if every node in the tree has the same value.
+
+Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        
+        main = root.val
+        def check(root, main):
+   
+            if root:
+                if root.val!= main:
+                    return False
+                
+                if not check(root.right,main) or not check(root.left,main):
+                    return False
+            return True
+            
+        
+        result = check(root,main)
+        return result
+        
