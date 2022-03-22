2b551c7622d34885c939a07d7d58a1677466c62e
Erjan <erjan@users.noreply.github.com>
2022-03-23 02:28:38 +0600

    Update longer_contiguous_segments_of_ones_than_zeros.py

--- longer_contiguous_segments_of_ones_than_zeros.py
+++ longer_contiguous_segments_of_ones_than_zeros.py
@@ -32,3 +32,12 @@ class Solution:
         print('zeros', zeros)
 
         return ones > zeros
+    
+    
+class Solution:
+    def checkZeroOnes(self, s: str) -> bool:
+        s1 = s.split('0')
+        s0 = s.split('1')
+        r1 = max([len(i) for i in s1])
+        r0 = max([len(i) for i in s0])
+        return r1>r0   
