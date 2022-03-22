299df56a5421fd4ea896e0a4bd26396fc08132af
Erjan <erjan@users.noreply.github.com>
2022-03-22 16:22:10 +0600

    Update three_divisors.py

--- three_divisors.py
+++ three_divisors.py
@@ -17,3 +17,21 @@ class Solution:
                 return True
             else:
                 return False
+
+            
+            
+class Solution:
+    def isThree(self, n: int) -> bool:
+        #check if exactly 1 divisor exists apart from 1 and number itself
+        if n <= 3:
+            return False
+        count = 0
+        for i in range(2,n//2 + 1):
+            #print(i)
+            if n % i == 0:
+                count += 1
+            if count > 1:
+                return False
+        if count == 0:
+            return False
+        return True            
