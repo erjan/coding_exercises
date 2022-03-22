2dbbecbaee24dd0e5a1fd64d6e4379e007d26440
Erjan <erjan@users.noreply.github.com>
2022-03-23 02:34:32 +0600

    Create longest_uncommon_subsequence_i.py

---
+++ longest_uncommon_subsequence_i.py
@@ -0,0 +1,16 @@
+'''
+Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If the longest uncommon subsequence does not exist, return -1.
+
+An uncommon subsequence between two strings is a string that is a subsequence of one but not the other.
+
+A subsequence of a string s is a string that can be obtained after deleting any number of characters from s.
+
+For example, "abc" is a subsequence of "aebdc" because you can delete the underlined characters in "aebdc" to get "abc". Other subsequences of "aebdc" include "aebdc", "aeb", and "" (empty string).
+'''
+
+#STUPID QUESTION!!!!!
+
+def findLUSlength(self, A, B):
+    if A == B:
+        return -1
+    return max(len(A), len(B))
