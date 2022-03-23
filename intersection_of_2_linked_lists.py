43258241c310fadd360a38904a6a6f2e7244cd05
Erjan <erjan@users.noreply.github.com>
2022-03-23 13:38:24 +0600

    Create intersection_of_2_linked_lists.py

---
+++ intersection_of_2_linked_lists.py
@@ -0,0 +1,28 @@
+'''
+Given the heads of two singly linked-lists headA and headB, 
+return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+'''
+
+class Solution:
+    # @param two ListNodes
+    # @return the intersected ListNode
+    def getIntersectionNode(self, headA, headB):
+        curA,curB = headA,headB
+        lenA,lenB = 0,0
+        while curA is not None:
+            lenA += 1
+            curA = curA.next
+        while curB is not None:
+            lenB += 1
+            curB = curB.next
+        curA,curB = headA,headB
+        if lenA > lenB:
+            for i in range(lenA-lenB):
+                curA = curA.next
+        elif lenB > lenA:
+            for i in range(lenB-lenA):
+                curB = curB.next
+        while curB != curA:
+            curB = curB.next
+            curA = curA.next
+        return curA
