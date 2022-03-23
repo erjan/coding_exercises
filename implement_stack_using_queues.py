05e1bd54f642cd2723bb9f24590dac3224224020
Erjan <erjan@users.noreply.github.com>
2022-03-24 04:45:44 +0600

    Update implement_stack_using_queues.py

--- implement_stack_using_queues.py
+++ implement_stack_using_queues.py
@@ -42,3 +42,31 @@ class MyStack:
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+
+#another solution from discussions
+
+class Stack:
+    # initialize your data structure here.
+    def __init__(self):
+        self.stack = collections.deque([])
+
+    # @param x, an integer
+    # @return nothing
+    def push(self, x):
+        self.stack.append(x)
+
+    # @return nothing
+    def pop(self):
+        for i in range(len(self.stack) - 1):
+            self.stack.append(self.stack.popleft())
+
+        self.stack.popleft()
+
+    # @return an integer
+    def top(self):
+        return self.stack[-1]
+
+    # @return an boolean
+    def empty(self):
+        return len(self.stack) == 0
