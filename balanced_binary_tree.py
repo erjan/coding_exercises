c251fd677d32990bf89deb85e21a5bb0ee0bf5c8
Erjan <erjan@users.noreply.github.com>
2022-03-24 08:25:36 +0600

    Update balanced_binary_tree.py

--- balanced_binary_tree.py
+++ balanced_binary_tree.py
@@ -31,5 +31,26 @@ class Solution:
         
         return self.isBalanced(root.left) and self.isBalanced(root.right)
     
+         
+class Solution(object):
+    def isBalanced(self, root):
+        stack, node, last, depths = [], root, None, {}
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack[-1]
+                if not node.right or last == node.right:
+                    node = stack.pop()
+                    left, right  = depths.get(node.left, 0), depths.get(node.right, 0)
+                    if abs(left - right) > 1: return False
+                    depths[node] = 1 + max(left, right)
+                    last = node
+                    node = None
+                else:
+                    node = node.right
+        return True
+         
     
         
