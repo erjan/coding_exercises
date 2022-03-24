e60d3e43934fdc90fa8367e8d96b1f47cb8c29eb
Erjan <erjan@users.noreply.github.com>
2022-03-24 12:49:19 +0600

    Update implement_queue_using_stacks.py

--- implement_queue_using_stacks.py
+++ implement_queue_using_stacks.py
@@ -37,3 +37,38 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+#another -------------------------------------------------------------------------
+
+class MyQueue:
+
+    def __init__(self):
+
+        self.stack1 = []
+        self.stack2 = []
+    def push(self, x: int) -> None:   
+        self.stack1.append(x)
+        
+
+    def pop(self) -> int:
+      
+        n = len(self.stack1) - 1
+        for i in range(n):
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1.pop()
+        for i in range(n):
+            self.stack1.append(self.stack2.pop())
+        return res
+
+    def peek(self) -> int:    
+        
+        n = len(self.stack1) - 1
+        for i in range(n):
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1[0]
+        for i in range(n):
+            self.stack1.append(self.stack2.pop())
+        return res
+
+    def empty(self) -> bool:     
+        return len(self.stack1) == 0
