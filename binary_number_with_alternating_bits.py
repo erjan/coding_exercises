4171daacb19cd5d095b9dde38839615e2ef73cd7
Erjan <erjan@users.noreply.github.com>
2022-03-24 10:15:17 +0600

    Create binary_number_with_alternating_bits.py

---
+++ binary_number_with_alternating_bits.py
@@ -0,0 +1,14 @@
+'''
+Given a positive integer, check whether it 
+has alternating bits: namely, if two adjacent bits will always have different values.
+'''
+
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+            n = bin(n)
+            n = n[2:]
+
+
+            res = not '00' in n and not '11' in n
+            print(res)
+            return res
