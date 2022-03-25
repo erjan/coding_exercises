f895e4fda6d1e325b0fe11d4b1b75b8d91975904
Erjan <erjan@users.noreply.github.com>
2022-03-25 15:23:02 +0600

    Create maximum_binary_tree.py

---
+++ maximum_binary_tree.py
@@ -0,0 +1,60 @@
+'''
+You are given an integer array nums with no duplicates. A maximum binary tree can be built recursively from nums using the following algorithm:
+
+Create a root node whose value is the maximum value in nums.
+Recursively build the left subtree on the subarray prefix to the left of the maximum value.
+Recursively build the right subtree on the subarray suffix to the right of the maximum value.
+Return the maximum binary tree built from nums.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        def helper(nums, root):            
+            if nums:
+            
+                cur_max = max(nums)
+                cur_max_index = nums.index(cur_max)
+
+                root = TreeNode(cur_max)
+
+                left_subarray = nums[:cur_max_index]
+                right_subarray = nums[cur_max_index+1:]
+
+                root.left = helper(left_subarray, root)
+                root.right = helper(right_subarray,root)            
+                return root            
+            else:
+                return None
+              
+        root = helper(nums, None)
+        return root
+      
+#-----------------------------------------------------------------      
+class Solution(object):
+    def constructMaximumBinaryTree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        stack = []  #build a decreasing stack
+        for i in nums:
+            node = TreeNode(i)
+            lastpop = None
+            
+            while stack and stack[-1].val < i:  #popping process
+                lastpop = stack.pop()
+            node.left = lastpop
+            
+            if stack:
+                stack[-1].right = node
+            stack.append(node)
+            
+        return stack[0]      
