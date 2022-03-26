33afada6d4db6c7a87e4b0c5ff4a03fa1919cd74
Erjan <erjan@users.noreply.github.com>
2022-03-26 16:39:18 +0600

    Create print_in_order.py

---
+++ print_in_order.py
@@ -0,0 +1,32 @@
+'''
+Suppose we have a class:
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+'''
+
+from threading import Lock
+
+class Foo:
+    def __init__(self):
+        self.locks = (Lock(),Lock())
+        self.locks[0].acquire()
+        self.locks[1].acquire()
+        
+    def first(self, printFirst):
+        printFirst()
+        self.locks[0].release()
+        
+    def second(self, printSecond):
+        with self.locks[0]:
+            printSecond()
+            self.locks[1].release()
+            
+            
+    def third(self, printThird):
+        with self.locks[1]:
+            printThird()
