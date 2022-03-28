e535f57b3a47b4b6640bb5939a71987a66a4c29b
Erjan <erjan@users.noreply.github.com>
2022-03-28 23:44:51 +0600

    Create read_n_characters_given_read4.py

---
+++ read_n_characters_given_read4.py
@@ -0,0 +1,28 @@
+'''
+Given a file and assume that you can only read the file using a given method read4, implement a method to read n characters.
+
+Method read4:
+
+The API read4 reads four consecutive characters from file, then writes those characters into the buffer array buf4.
+
+The return value is the number of actual characters read.
+
+Note that read4() has its own file pointer, much like FILE *fp in C.
+'''
+
+class Solution:
+    def read(self, buf: List[str], n: int) -> int:
+        copied_chars = 0
+        read_chars = 4
+        buf4 = [''] * 4
+        
+        while copied_chars < n and read_chars == 4:
+            read_chars = read4(buf4)
+            
+            for i in range(read_chars):
+                if copied_chars == n:
+                    return copied_chars
+                buf[copied_chars] = buf4[i]
+                copied_chars += 1
+        
+        return copied_chars
