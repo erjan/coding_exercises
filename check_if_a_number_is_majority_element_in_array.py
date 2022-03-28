d2408c964e0c16f003ffa2f7fb02449c14acce8c
Erjan <erjan@users.noreply.github.com>
2022-03-28 20:44:31 +0600

    Update check_if_a_number_is_majority_element_in_array.py

--- check_if_a_number_is_majority_element_in_array.py
+++ check_if_a_number_is_majority_element_in_array.py
@@ -37,3 +37,21 @@ def isMajorityElement(self, nums, target):
         lo = search(nums, target)
         hi = search(nums, target + 1)
         return hi - lo > N // 2
+
+    
+    
+#-----------------------------
+During an interview, we would think a bit more. Letting x be our majority element, a sorted array containing a majority element would look like one of the following:
+
+[ x x x x x x x . . . . . . ]        # majority at the beginning
+[ . . . x x x x x x x . . . ]        # majority at the middle
+[ . . . . . . x x x x x x x ]        # majority at the ending
+If there is a majority element then when we examine the middle index of the array, we are guaranteed to find the majority element. So we can binary search for the beginning and end of the streak:
+
+def isMajorityElement(self, nums, target):
+    N = len(nums)
+    if nums[N // 2] != target:
+        return False
+    lo = bisect.bisect_left(nums, target)
+    hi = bisect.bisect_right(nums, target)
+    return hi - lo > N // 2
