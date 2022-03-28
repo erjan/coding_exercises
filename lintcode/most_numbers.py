ac9d781838cff4d8ed7dc7bebe8db0b45af0d360
Erjan <erjan@users.noreply.github.com>
2022-03-28 11:49:39 +0600

    Update most_numbers.py

--- lintcode/most_numbers.py
+++ lintcode/most_numbers.py
@@ -3,22 +3,28 @@ Find the number with the most occurrences in the given array.
 When the number of occurrences is the same, return the smallest one.
 '''
 
-def f(array):     
-      num2cnt = dict()
-      for num in array:
-          if num in num2cnt.keys():
-              num2cnt[num] += 1
-          else:
-              num2cnt[num] = 1
-      max_num = -1
-      res = -1
-      for k, v in num2cnt.items():
-          if v > max_num:
-              max_num = v
-              res = k
-          elif v == max_num and k < res:
-              res = k
-      return res
+from collections import Counter
+
+class Solution:
+  
+    def find_number(self, array: List[int]) -> int:
+        # Write your code here.
+
+        d = dict(Counter(array))
+
+        maxval = -1
+        maxkey = -1
+
+        for k,v in d.items():
+
+            if v > maxval:
+                maxval = v
+                maxkey = k
+            elif v == maxval and k < maxkey:
+                maxval = v
+                maxkey = k
+        return maxkey
+
     
 #my solution
 
