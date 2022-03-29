86d84957d1af2ffd7474d48c543d91f14f28b938
Erjan <erjan@users.noreply.github.com>
2022-03-29 23:50:43 +0600

    Update employees_whose_manager_left_the_company.sql

--- employees_whose_manager_left_the_company.sql
+++ employees_whose_manager_left_the_company.sql
@@ -8,11 +8,18 @@ The query result format is in the following example.
 
 
 
-SELECT DISTINCT emp.employee_id
-FROM   employees emp
-       LEFT JOIN employees mng
-              ON( emp.manager_id = mng.employee_id )
-WHERE  emp.manager_id IS NOT NULL
-       AND emp.salary < 30000
-       AND mng.employee_id IS NULL
-ORDER  BY emp.employee_id 
+SELECT DISTINCT e1.employee_id
+
+FROM employees e1
+
+LEFT JOIN employees e2
+
+ON e1.manager_id = e2.employee_id 
+              
+WHERE  e1.manager_id IS NOT NULL
+
+       AND e1.salary < 30000
+       
+       AND e2.employee_id IS NULL
+       
+ORDER  BY e1.employee_id 
