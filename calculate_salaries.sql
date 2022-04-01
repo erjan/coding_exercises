39bc4568dfce0523d4c2b0728e2d7f5620b8378d
Erjan <erjan@users.noreply.github.com>
2022-04-01 21:57:14 +0600

    Update calculate_salaries.sql

--- calculate_salaries.sql
+++ calculate_salaries.sql
@@ -46,3 +46,17 @@ SELECT company_id, employee_id, employee_name
     ELSE salary-(salary*.49)
 END,0) salary
 FROM salaries
+
+
+
+#another similar
+
+select s.company_id, s.employee_id, s.employee_name,
+round(
+		case when x.max_sal between 1000 and 10000 then salary * 0.76
+		when x.max_sal > 10000 then salary * 0.51 else salary end, 0) as salary
+
+from salaries s inner join
+(select company_id, max(salary) max_sal from salaries group by company_id) x
+
+on s.company_id = x.company_id;
