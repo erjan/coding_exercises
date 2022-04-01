4be6cf54f00c90df0b41a2973c223d7f15d6a198
Erjan <erjan@users.noreply.github.com>
2022-04-01 22:12:59 +0600

    Update game_play_analysis_ii.sql

--- game_play_analysis_ii.sql
+++ game_play_analysis_ii.sql
@@ -23,3 +23,16 @@ where (player_id, event_date) in
 group by player_id
 order by event_date asc)
 
+
+
+#another
+
+SELECT a.player_id
+        ,a.device_id
+FROM 
+    (SELECT player_id
+            ,min(event_date) AS min_date
+    FROM Activity
+    GROUP BY player_id) dt
+JOIN Activity a ON a.player_id = dt.player_id
+AND a.event_date = dt.min_date
