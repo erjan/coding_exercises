965820efe903b277c6219d16b2ac8856b6e4f7d8
Erjan <erjan@users.noreply.github.com>
2022-04-06 03:47:04 +0600

    Create unpopular_books.sql

---
+++ unpopular_books.sql
@@ -0,0 +1,22 @@
+
+'''
+
+Write an SQL query that reports the books that have sold less than 10 copies in the last year, excluding books that have been available for less than one month from today. Assume today is 2019-06-23.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+'''
+
+select b.book_id, b.name
+
+from books b left join orders o
+
+on b.book_id = o.book_id and dispatch_date between '2018-06-23' and '2019-06-23'
+
+where datediff('2019-06-23', available_from) > 30
+
+group by b.book_id, b.name
+
+having ifnull(sum(quantity),0) <10;
