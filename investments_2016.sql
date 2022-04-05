be92c42ef88d7d6e701e82a0cb2c551571248c27
Erjan <erjan@users.noreply.github.com>
2022-04-06 03:37:12 +0600

    Update investments_2016.sql

--- investments_2016.sql
+++ investments_2016.sql
@@ -15,3 +15,10 @@ select round(sum(TIV_2016),2) as TIV_2016
 from insurance i
 where (select count(*) from insurance i1 where i.TIV_2015 = i1.TIV_2015)>1 and 
       (select count(*) from insurance i2 where (i.LAT, i.LON) = (i2.LAT, i2.LON))=1
+
+
+#another
+
+SELECT ROUND(SUM(TIV_2016),2) AS TIV_2016 FROM Insurance a
+WHERE EXISTS (SELECT * FROM Insurance WHERE PID <> a.PID AND TIV_2015 = a.TIV_2015)
+AND NOT EXISTS (SELECT * FROM Insurance WHERE PID <> a.PID AND (LAT,LON) = (a.LAT,a.LON));
