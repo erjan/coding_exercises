8fcaffde853266c94f4f153db8f1e96355141903
Erjan <erjan@users.noreply.github.com>
2022-04-10 04:25:45 +0600

    Update sentence_screen_fitting.py

--- sentence_screen_fitting.py
+++ sentence_screen_fitting.py
@@ -44,3 +44,19 @@ class Solution(object):
                 total_sentences_ct += complete_sentences_ct
             curr_row += 1
         return total_sentences_ct
+
+    
+--------------------------------------
+
+class Solution:
+    def wordsTyping(self, sentence: List[str], rows: int, cols: int) -> int:
+        str=" ".join(sentence)+" "
+        valid_space=0
+        for i in range(rows):
+            valid_space+=cols
+            if str[valid_space%len(str)]==" ":
+                valid_space+=1
+            else:
+                while valid_space>0 and str[valid_space%len(str)-1]!=" ":
+                    valid_space-=1
+        return valid_space//len(str)
