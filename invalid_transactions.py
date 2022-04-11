e19fbd91538db5a153c6539b3c7dbf70d02be5b4
Erjan <erjan@users.noreply.github.com>
2022-04-11 21:49:15 +0600

    Update invalid_transactions.py

--- invalid_transactions.py
+++ invalid_transactions.py
@@ -121,4 +121,54 @@ class Solution(object):
                     invalid.append(t)
                     break
         return invalid
-
+---------------------------------------------------------
+class Solution(object):
+    def invalidTransactions(self, transactions):
+        """
+        :type transactions: List[str]
+        :rtype: List[str]
+        """
+        
+        r = {}
+                
+        inv = []        
+        for i in transactions:
+            split = i.decode("utf-8").split(",")
+            name = str(split[0])
+            time = int(split[1])
+            amount = int(split[2])
+            city = str(split[3])
+            
+            if time not in r:
+                r[time] = {
+                    name: [city]
+                }
+            else:
+                if name not in r[time]:
+                    r[time][name]=[city]
+                else:
+                    r[time][name].append(city)
+                    
+        
+        for i in transactions:
+            split = i.decode("utf-8").split(",")
+            name = str(split[0])
+            time = int(split[1])
+            amount = int(split[2])
+            city = str(split[3])
+            
+            
+            if amount > 1000:
+                inv.append(i)
+                continue
+            
+            for j in range(time-60, time+61):
+                if j not in r:
+                    continue
+                if name not in r[j]:
+                    continue
+                if len(r[j][name]) > 1 or (r[j][name][0] != city):
+                    inv.append(i)
+                    break
+                                        
+        return inv   
