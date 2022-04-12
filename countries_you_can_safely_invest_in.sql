c860246b121415c879b67336c35d8c6a2611cd9a
Erjan <erjan@users.noreply.github.com>
2022-04-12 07:44:03 +0600

    Update countries_you_can_safely_invest_in.sql

--- countries_you_can_safely_invest_in.sql
+++ countries_you_can_safely_invest_in.sql
@@ -39,3 +39,19 @@ from(
     inner join country c on substring(p.phone_number,1,3) = c.country_code
 ) a
 where avg_duration > avg_total
+
+----------------------------
+
+SELECT
+ co.name AS country
+FROM person p 
+INNER JOIN country co
+     ON LEFT(phone_number,3) = country_code
+     
+ INNER JOIN calls c
+     ON p.id = c.caller_id
+     OR p.id = c.callee_id
+GROUP BY
+ co.name
+HAVING
+ AVG(duration) > (SELECT AVG(duration) FROM calls)
