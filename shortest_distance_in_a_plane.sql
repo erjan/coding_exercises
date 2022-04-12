7023bd068d772215a5241258e2535ec58b0644ad
Erjan <erjan@users.noreply.github.com>
2022-04-12 06:15:58 +0600

    Update shortest_distance_in_a_plane.sql

--- shortest_distance_in_a_plane.sql
+++ shortest_distance_in_a_plane.sql
@@ -16,3 +16,14 @@ round(sqrt(power((p2.x - p1.x),2) + power((p2.y - p1.y),2) ),2) as shortest
 from point2d p1 cross join point2d p2
 where p1.x != p2.x or p2.y != p1.y
  order by shortest asc limit 1
+
+
+
+--------------------
+select
+round(sqrt(power(p2.x-p1.x,2) + power(p2.y-p1.y,2)),2) as shortest
+from
+point_2d p1 cross join point_2d p2
+on (p1.x,p1.y) != (p2.x,p2.y)
+order by 1 asc
+limit 1;
