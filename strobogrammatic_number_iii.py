903bf35ea9c835ec96ddd454ebefdeb7eccd2a55
Erjan <erjan@users.noreply.github.com>
2022-04-17 22:24:47 +0600

    Create strobogrammatic_number_iii.py

---
+++ strobogrammatic_number_iii.py
@@ -0,0 +1,122 @@
+Given two strings low and high that represent two integers 
+low and high where low <= high, return the number of strobogrammatic numbers in the range [low, high].
+
+A strobogrammatic number is a number that looks the same when 
+rotated 180 degrees (looked at upside down).
+
+
+
+class Solution(object):
+    def strobogrammaticInRange(self, low, high):
+        """
+        :type low: str
+        :type high: str
+        :rtype: int
+        """
+        l, u = len(low), len(high)
+        ans = []
+        for n in range(l, u+1): 
+            self.dfs(0, n-1, ["" for _ in range(n)], ans, n, int(low), int(high))
+        return len(ans)
+        
+    def dfs(self, currentIndex, endIndex, ansList, new_ans, n, low_limit, upper_limit):
+        if currentIndex > endIndex:
+            ans = ''.join(ansList[:])
+            if int(ans) >= low_limit and int(ans) <= upper_limit:
+                new_ans.append(ans)
+            return 
+        if currentIndex == endIndex:
+            for letter in ['0', '1', '8']:
+                ansList[currentIndex] = letter
+                ans = ''.join(ansList[:])
+                if int(ans) >= low_limit and int(ans) <= upper_limit:
+                    new_ans.append(ans)
+            return 
+        
+        
+        for letter in ['0', '1', '6', '8', '9']:
+            if currentIndex != 0:
+                if letter == '0':
+                    ansList[currentIndex] = '0'
+                    ansList[endIndex] = '0'
+                    self.dfs(currentIndex + 1, endIndex - 1, ansList, new_ans, n, low_limit, upper_limit)
+                elif letter == '1':
+                    ansList[currentIndex] = '1'
+                    ansList[endIndex] = '1'
+                    self.dfs(currentIndex + 1, endIndex - 1, ansList, new_ans, n, low_limit,upper_limit)
+                elif letter == '8':
+                    ansList[currentIndex] = '8'
+                    ansList[endIndex] = '8'
+                    self.dfs(currentIndex + 1, endIndex - 1, ansList, new_ans, n, low_limit,upper_limit)
+                elif letter == '6':
+                    ansList[currentIndex] = '6'
+                    ansList[endIndex] = '9'
+                    self.dfs(currentIndex + 1, endIndex - 1, ansList, new_ans, n, low_limit,upper_limit)
+                else:
+                    ansList[currentIndex] = '9'
+                    ansList[endIndex] = '6'
+                    self.dfs(currentIndex + 1, endIndex - 1, ansList, new_ans, n, low_limit,upper_limit)
+            
+            else:
+                if letter == '1':
+                    ansList[currentIndex] = '1'
+                    ansList[endIndex] = '1'
+                    self.dfs(currentIndex + 1, endIndex - 1, ansList, new_ans, n,low_limit, upper_limit)
+                elif letter == '8':
+                    ansList[currentIndex] = '8'
+                    ansList[endIndex] = '8'
+                    self.dfs(currentIndex + 1, endIndex - 1, ansList, new_ans, n, low_limit,upper_limit)
+                elif letter == '6':
+                    ansList[currentIndex] = '6'
+                    ansList[endIndex] = '9'
+                    self.dfs(currentIndex + 1, endIndex - 1, ansList, new_ans, n, low_limit,upper_limit)
+                elif letter == '9':
+                    ansList[currentIndex] = '9'
+                    ansList[endIndex] = '6'
+                    self.dfs(currentIndex + 1, endIndex - 1, ansList, new_ans, n, low_limit,upper_limit)
+                else: 
+                    continue 
+                    
+-----------------------------------------------------------------------           
+
+
+O((5 ^ (n/2)) - where N is the character length of high
+Space: recursion stack O((5 ^ (n/2)) - where N is the character length of high
+Algorithm: use result from LC#247 and construct all the strobogrammatic numbers in length low to high, then count the ones which are within the specified range.
+
+
+def strobogrammaticInRange(self, low, high):
+        """
+        :type low: str
+        :type high: str
+        :rtype: int
+        """
+        all_strobo = []
+        for length in range(len(low), len(high) + 1):
+            all_strobo += self.strobo(length)
+        res = 0
+        for num in all_strobo:
+            if int(low) <= int(num) <= int(high):
+                res += 1
+        return res
+        
+    def strobo(self, n):
+        self.mapping = {"0": "0", "1": "1", "6": "9", "9": "6", "8": "8"}
+        self.res = []
+        if n % 2:
+            for i in ["0", "8", "1"]:
+                self.dfs(str(i), n - 1)
+        else:
+            self.dfs("", n)
+        return self.res
+        
+    def dfs(self, num, n):
+        if n == 0:
+            self.res.append(num)
+            return
+            
+        for number in self.mapping:
+            if n == 2 and number != "0":
+                self.dfs(number + num + self.mapping[number], n - 2)
+            elif n > 2:
+                self.dfs(number + num + self.mapping[number], n - 2)                    
