0d5980b4b0b13204fadf3779ae28aebe787bc483
Erjan <erjan@users.noreply.github.com>
2022-04-17 12:14:18 +0600

    Update consecutive_empty_boxes.sql

--- lintcode/consecutive_empty_boxes.sql
+++ lintcode/consecutive_empty_boxes.sql
@@ -11,3 +11,15 @@ from  boxes a,
 where abs(a.id-b.id)=1
 and (a.is_empty=1 and b.is_empty=1)
 order by a.id asc
+
+
+
+-----------------------------------------
+
+select id from boxes where id in (
+    select b1.id from boxes b1,boxes b2
+    where (b1.is_empty = 1 and b2.is_empty=1 and ( 
+         b1.id + 1= b2.id or b2.id +1 = b1.id
+      )
+    ) 
+) order by id
