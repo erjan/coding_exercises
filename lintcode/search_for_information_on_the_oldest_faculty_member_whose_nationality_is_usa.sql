76a7d8b55d2a3d6908fdb70faff6d047cfda2459
Erjan <erjan@users.noreply.github.com>
2022-04-17 14:59:12 +0600

    Update search_for_information_on_the_oldest_faculty_member_whose_nationality_is_usa.sql

--- lintcode/search_for_information_on_the_oldest_faculty_member_whose_nationality_is_usa.sql
+++ lintcode/search_for_information_on_the_oldest_faculty_member_whose_nationality_is_usa.sql
@@ -6,3 +6,10 @@ Write an SQL statement to query the information of the oldest teacher who come f
 
 SELECT * from teachers 
 where age =(SELECT max(age) from teachers) and country = 'USA';
+
+
+
+
+----------------------------------------------------------------------------
+select * from teachers where age = (select max(age) from teachers)
+and country = 'USA'
