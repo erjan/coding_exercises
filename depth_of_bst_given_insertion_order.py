461351195d40911bf8129427f03f77d87a9bc720
Erjan <erjan@users.noreply.github.com>
2022-04-19 16:12:47 +0600

    Update depth_of_bst_given_insertion_order.py

--- depth_of_bst_given_insertion_order.py
+++ depth_of_bst_given_insertion_order.py
@@ -109,4 +109,20 @@ class Solution:
         # return the maximum value so far
         return max(depths.values())
       
+	
+--------------------------------------------------------------------
+class Solution:
+    def maxDepthBST(self, order: List[int]) -> int:
+        que = []
+        depths = {}
+        for val in order:
+            res = 0
+            idx = bisect.bisect(que, val)
+            if idx != 0:
+                res = max(res, depths[que[idx - 1]])
+            if idx != len(que):
+                res = max(res, depths[que[idx]])
+            que.insert(idx, val)
+            depths[val] = res + 1
+        return max(depths.values())
       
