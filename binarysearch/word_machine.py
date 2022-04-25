1504be16606272390791a553e2c8232e0957c709
Erjan <erjan@users.noreply.github.com>
2022-04-26 02:05:46 +0600

    Update word_machine.py

--- binarysearch/word_machine.py
+++ binarysearch/word_machine.py
@@ -53,3 +53,23 @@ class Solution:
         else:
             return -1
             
+            
+-----------------------------------------------------------------------------------------------
+class Solution:
+    def solve(self, ops):
+        stack = []
+        try:
+            for n in ops:
+                if n == "DUP":
+                    stack.append(stack[-1])
+                elif n == "POP":
+                    stack.pop()
+                elif n == "-":
+                    stack.append(stack.pop() - stack.pop())
+                elif n == "+":
+                    stack.append(stack.pop() + stack.pop())
+                else:
+                    stack.append(int(n))
+            return stack[-1]
+        except:
+            return -1
