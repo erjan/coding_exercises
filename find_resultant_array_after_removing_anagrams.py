1c5948a6062060eaa53b220a9a26866ec7f06a05
Erjan <erjan@users.noreply.github.com>
2022-07-21 09:31:48 +0500

    Update find_resultant_array_after_removing_anagrams.py

--- find_resultant_array_after_removing_anagrams.py
+++ find_resultant_array_after_removing_anagrams.py
@@ -21,3 +21,13 @@ class Solution:
                 continue
             i += 1
         return words
+
+------------------------------------------------------------
+from collections import Counter
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        res = [words[0]]
+        for i in range(1,len(words)):
+            if Counter(res[-1]) != Counter(words[i]):
+                res.append(words[i])
+        return res
