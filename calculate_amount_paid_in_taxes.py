6cc4f09bddd166c55727cd8c15e2659cf494da0d
Erjan <erjan@users.noreply.github.com>
2022-07-22 20:10:41 +0500

    Update calculate_amount_paid_in_taxes.py

--- calculate_amount_paid_in_taxes.py
+++ calculate_amount_paid_in_taxes.py
@@ -24,3 +24,53 @@ class Solution:
             prev = k
             
         return total_tax
+
+    
+-------------------------------------------------------------------------------
+
+Zewei_Liu's avatar
+Zewei_Liu
+0
+June 14, 2022 6:23 PM
+
+27 VIEWS
+
+This problem could be solved with 2-step approach.
+
+Spliting income to each tax bracket.
+Calculating tax in each income split.
+Simple Python3 code below:
+
+class Solution:
+    def calculateTax(self, brackets: List[List[int]], income: int) -> float:
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        """
+        tax = 0 
+        pre_upper = 0
+        for tax_bracket in brackets:
+            upper = tax_bracket[0]
+            percent = tax_bracket[1]
+            split_income = upper - pre_upper if income >= upper else income - pre_upper
+            tax += split_income * percent * 0.01
+            if income < upper:
+                break
+            pre_upper = upper
+        
+        return tax
+    
+----------------------------------------------------------------------------------------------
+class Solution:
+    def calculateTax(self, brackets: List[List[int]], income: int) -> float:
+        lower = 0; 
+		tax = 0; 
+		left = income # amount left to be taxed
+        for i in brackets:
+            k = i[0]-lower # amount being taxed
+            if k<= left:
+                tax+=k*i[1]/100;  left-=k;  lower=i[0]
+            else:
+                tax+= left*i[1]/100
+                break
+        return tax
