f802e8c5df2fbb76d74ea63fb7e7715ddc6eb940
Erjan <erjan@users.noreply.github.com>
2022-08-06 20:48:37 +0500

    Update find_the_duplicate_number.py

--- find_the_duplicate_number.py
+++ find_the_duplicate_number.py
@@ -17,3 +17,35 @@ class Solution:
                 d[nums[i]] = 1
             else:
                 return nums[i]
+
+            
+----------------------------------------------------------------
+#Array as HashMap
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        while nums[0] != nums[nums[0]]:
+            nums[nums[0]], nums[0] = nums[0], nums[nums[0]]
+        return nums[0]
+--------------------------------------------------------------------------------     
+#binary search
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # 'low' and 'high' represent the range of values of the target
+        low = 1
+        high = len(nums) - 1
+        
+        while low <= high:
+            cur = (low + high) // 2
+            count = 0
+
+            # Count how many numbers are less than or equal to 'cur'
+            count = sum(num <= cur for num in nums)
+            if count > cur:
+                duplicate = cur
+                high = cur - 1
+            else:
+                low = cur + 1
+                
+        return duplicate
