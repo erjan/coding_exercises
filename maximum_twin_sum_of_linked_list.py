7ba66c808837e90c72b65889752cd7f9a5e9fe05
Erjan <erjan@users.noreply.github.com>
2022-08-15 08:43:28 +0500

    Update maximum_twin_sum_of_linked_list.py

--- maximum_twin_sum_of_linked_list.py
+++ maximum_twin_sum_of_linked_list.py
@@ -56,4 +56,33 @@ def pairSum(self, head: Optional[ListNode]) -> int:
 
 	return maxVal
 
+
+----------------------------------------------------------------------------
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        def reverse(head):
+            prev, curr = None, head
+            while curr:
+                next_node = curr.next
+                curr.next = prev
+                prev, curr = curr, next_node
+            return prev
+        
+        slow = head
+        fast = head
+        
+        while fast:
+            slow = slow.next
+            fast = fast.next.next
+        
+        first = head
+        second = reverse(slow)
+        max_so_far = 0
+        
+        while second:
+            summ = first.val + second.val
+            max_so_far = max(max_so_far, summ)
+            first, second = first.next, second.next
+        
+        return max_so_far
           
