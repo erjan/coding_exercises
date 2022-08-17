4d35849f7451a5b83d9c548d726bdb2ce89d0734
Erjan <erjan@users.noreply.github.com>
2022-08-17 18:44:11 +0500

    Create print_foobar_alternatively.py

---
+++ print_foobar_alternatively.py
@@ -0,0 +1,32 @@
+'''
+The same instance of FooBar will be passed to two different threads:
+
+thread A will call foo(), while
+thread B will call bar().
+Modify the given program to output "foobar" n times.
+'''
+
+
+from threading import Lock
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.foo_lock = Lock()
+        self.bar_lock = Lock()
+        self.bar_lock.acquire()
+
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.foo_lock.acquire()
+            # printFoo() outputs "foo". Do not change or remove this line.
+            printFoo()
+            self.bar_lock.release()
+
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.bar_lock.acquire()
+            # printBar() outputs "bar". Do not change or remove this line.
+            printBar()
+            self.foo_lock.release()
