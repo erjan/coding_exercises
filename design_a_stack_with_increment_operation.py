2751aa9878a6a94dbacb93f2fd01d53b7ec1d1ab
Erjan <erjan@users.noreply.github.com>
2022-08-19 19:15:48 +0500

    Update design_a_stack_with_increment_operation.py

--- design_a_stack_with_increment_operation.py
+++ design_a_stack_with_increment_operation.py
@@ -48,3 +48,96 @@ class CustomStack:
         else:
             for i in range(k):
                 self.main[i] = self.main[i] + val
+                
+----------------------------------------------------------------------------------------------
+# sol-n using 2 stacks(lists)
+
+def __init__(self, maxSize):
+        self.n = maxSize
+        self.stack = []
+        self.inc = []
+
+    def push(self, x):
+        if len(self.inc) < self.n:
+            self.stack.append(x)
+            self.inc.append(0)
+
+    def pop(self):
+        if not self.inc: return -1
+        if len(self.inc) > 1:
+            self.inc[-2] += self.inc[-1]
+        return self.stack.pop() + self.inc.pop()
+
+    def increment(self, k, val):
+        if self.inc:
+            self.inc[min(k, len(self.inc)) - 1] += val
+            
+-------------------------------------------------------------------------------------------
+class Entry:
+    def __init__(self, value, offset ):
+        
+        self.value = value
+        self.offset = offset
+
+        
+    def __repr__(self):
+        # override __repr__ to help programmer trace and debug
+        msg = [ f'value = {self.value}', f'offset = {self.offset}', '---']
+        return '\n'.join( msg )
+        
+        
+        
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        
+        self.stk = []
+        self.size_limit = maxSize
+
+        
+        
+    def push(self, x: int) -> None:
+
+        
+        if len(self.stk) < self.size_limit:
+            
+            # push new element when stack size is within limit
+            self.stk.append( Entry( value = x, offset = 0) )
+
+
+        
+    def pop(self) -> int:
+        
+        if self.stk:
+            
+            # fetch and pop top element from stack
+            top_element = self.stk.pop()
+            
+            if self.stk:
+                # If stack is still non-empty, 
+                # propagate increment offset to lower level
+                self.stk[-1].offset += top_element.offset
+            
+
+            # compute result with increment offset
+            return top_element.value + top_element.offset
+        
+        else:
+            
+            # stack is empty, directly return -1
+            return -1
+
+        
+        
+    def increment(self, k: int, val: int) -> None:
+        
+
+        if self.stk:
+            
+            # update offset at k-th element when stack is non-empty
+            
+            # adjust k if k is over stack size
+            k = min( k, len(self.stk) )
+            
+            self.stk[k-1].offset += val
+        
