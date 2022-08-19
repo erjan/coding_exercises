f5855cb60ba8f49f38fcbb23f1b299f6e12c817f
Erjan <erjan@users.noreply.github.com>
2022-08-19 09:49:17 +0500

    Create human_traffic_of_stadium.sql

---
+++ human_traffic_of_stadium.sql
@@ -0,0 +1,22 @@
+'''
+Write an SQL query to display the records with three or more rows with consecutive id's, and the number of people is greater than or equal to 100 for each.
+
+Return the result table ordered by visit_date in ascending order.
+
+The query result format is in the following example.
+'''
+
+
+select distinct t1.*
+from stadium t1, stadium t2, stadium t3
+where t1.people >= 100 and t2.people >= 100 and t3.people >= 100
+and
+(
+    (t1.id - t2.id = 1 and t1.id - t3.id = 2 and t2.id - t3.id =1)  -- t1, t2, t3
+    or
+    (t2.id - t1.id = 1 and t2.id - t3.id = 2 and t1.id - t3.id =1) -- t2, t1, t3
+    or
+    (t3.id - t2.id = 1 and t2.id - t1.id =1 and t3.id - t1.id = 2) -- t3, t2, t1
+)
+order by t1.id
+;
