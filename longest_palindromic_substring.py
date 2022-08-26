1496c3089321601d9e8507e8bd5cf8eedff9aeff
Erjan <erjan@users.noreply.github.com>
2022-08-26 23:19:54 +0500

    Update longest_palindromic_substring.py

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -26,4 +26,26 @@ class Solution:
                 r = r+1
         return res
             
+------------------------------------------
+#dp
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+        longest_palindrome_start, longest_palindrome_len = 0, 1
+
+        for end in range(0, n):
+            for start in range(end - 1, -1, -1):
+                # print('start: %s, end: %s' % (start, end))
+                if s[start] == s[end]:
+                    if end - start == 1 or dp[start + 1][end - 1]:
+                        dp[start][end] = True
+                        palindrome_len = end - start + 1
+                        if longest_palindrome_len < palindrome_len:
+                            longest_palindrome_start = start
+                            longest_palindrome_len = palindrome_len
+        return s[longest_palindrome_start: longest_palindrome_start + longest_palindrome_len]
             
