6d8b31c5c93b2f1d87d58ef0d447642ab9c9564f
Erjan <erjan@users.noreply.github.com>
2022-08-29 20:00:56 +0500

    Update rotate_image.py

--- rotate_image.py
+++ rotate_image.py
@@ -11,3 +11,33 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         matrix[:] = zip(*matrix[::-1])
+
+        
+----------------------------------------------------------------------
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
+-----------------------------------------------------------------------------------------
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        self.transpose(matrix)
+        self.reflect(matrix)
+    
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
