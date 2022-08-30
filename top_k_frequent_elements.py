8445cf43e90be3c39641c1822e7184c37f4a3e0d
Erjan <erjan@users.noreply.github.com>
2022-08-31 00:08:02 +0500

    Update top_k_frequent_elements.py

--- top_k_frequent_elements.py
+++ top_k_frequent_elements.py
@@ -10,3 +10,55 @@ class Solution:
 
         return res
         
+
+        
+---------------------------------------------------------------------------------
+class Solution:
+	def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+		frequency = {}
+
+		for num in nums:
+
+			if num not in frequency:
+
+				frequency[num] = 1
+
+			else:
+
+				frequency[num] = frequency[num] + 1
+
+		frequency = dict(sorted(frequency.items(), key=lambda x: x[1], reverse=True))
+
+		result = list(frequency.keys())[:k]
+
+		return result
+    
+----------------------------------------------------------------------------------------------
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq_table = {}
+        for i in nums:
+            freq_table[i] = freq_table.get(i, 0) + 1
+        heap = []
+        for i in freq_table.keys():
+            if len(heap) == k: # If size is k then we dont want to increase the size further 
+                heappushpop(heap, (freq_table[i], i))
+            else: # Size is not k then freely push values
+                heappush(heap, (freq_table[i], i))
+		# After this operation the heap contains only k largest values of all the values in nums
+        ans = []
+        while k > 0:
+            k -= 1
+            ans.append(heappop(heap)[1])
+        return ans
+    
+----------------------------------
+class Solution:
+    def topKFrequent(self, nums, k) :
+        map=Counter(nums) # counter is used to count the frequency of each element
+        result=[]
+        for key,value in map.items():
+            result.append([key,value])
+        result.sort(key=lambda x:x[1],reverse=True) # sort with respect to the second element in the list
+        return [x[0] for x in result[:k]] # return the first k elements in the list
