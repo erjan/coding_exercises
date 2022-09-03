a0df78c7b4eafe3587e2edc1c85bc5185c38b495
Erjan <erjan@users.noreply.github.com>
2022-09-04 04:53:32 +0500

    Update encode_and_decode_strings.py

--- encode_and_decode_strings.py
+++ encode_and_decode_strings.py
@@ -203,5 +203,34 @@ Overkill for this problem but I thought it was worth showing.
 --------------------------------------------------      
       
         
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        # write your code here
+        res = ''
+        for s in strs:
+
+            res += str(len(s)) + '#' + s
+        return res
+
+    """
+    @param: str: A string
+    @return: dcodes a single string to a list of strings
+    """
+    def decode(self, str):
+        # write your code here
+        res = []
+        i = 0
+        while i < len(str):
+            j = i
+            while str[j] != '#':
+                j+=1
+            length = int(str[i:j])
+            res.append(str[j+1:j+1+length])
+            i = j+1+length
+        return res
       
       
