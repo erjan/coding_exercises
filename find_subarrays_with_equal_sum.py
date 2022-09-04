6c867cb3fb401ea4471ce7ac2b28fcf609eca189
Erjan <erjan@users.noreply.github.com>
2022-09-05 01:40:21 +0500

    Update find_subarrays_with_equal_sum.py

--- find_subarrays_with_equal_sum.py
+++ find_subarrays_with_equal_sum.py
@@ -19,3 +19,17 @@ class Solution:
                 if first == second:
                     return True
         return False
+-----------------------------------------------------------------------------
+
+class Solution:
+    def findSubarrays(self, nums: List[int]) -> bool:
+        sums = set()
+        
+        for i in range(len(nums)-1):
+            t = nums[i]+nums[i+1]
+            if t in sums:
+                return True
+            else:
+                sums.add(t)
+            
+        return False
