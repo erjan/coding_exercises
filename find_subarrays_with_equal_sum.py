b9153af557a7ecb7400c698132514354d3edb542
Erjan <erjan@users.noreply.github.com>
2022-09-05 01:38:33 +0500

    Create find_subarrays_with_equal_sum.py

---
+++ find_subarrays_with_equal_sum.py
@@ -0,0 +1,21 @@
+'''
+Given a 0-indexed integer array nums, determine whether 
+there exist two subarrays of length 2 with equal sum. Note that the two subarrays must begin at different indices.
+
+Return true if these subarrays exist, and false otherwise.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+'''
+
+
+class Solution:
+    def findSubarrays(self, nums: List[int]) -> bool:
+        
+        for i in range(len(nums)-2):
+            first = nums[i] + nums[i+1]
+
+            for j in range(i+1, len(nums)-1):
+                second = nums[j] + nums[j+1]
+                if first == second:
+                    return True
+        return False
