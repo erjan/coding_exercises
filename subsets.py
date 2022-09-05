c5c380ed1984a9a6b08073af51d0d91b066fd709
Erjan <erjan@users.noreply.github.com>
2022-09-05 15:50:41 +0500

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -18,4 +18,22 @@ class Solution:
         res.append(nums)
         return res
     
+----------------------------------------------------------------------
+class Solution(object):
+    def subsets(self, nums):
+        nums.sort()
+        result = [[]]
+        for num in nums:
+            result += [i + [num] for i in result]
+        return result
         
+-----------------------------------------------------------------------------------
+def subsets1(self, nums):
+    res = []
+    self.dfs(sorted(nums), 0, [], res)
+    return res
+    
+def dfs(self, nums, index, path, res):
+    res.append(path)
+    for i in xrange(index, len(nums)):
+        self.dfs(nums, i+1, path+[nums[i]], res)
