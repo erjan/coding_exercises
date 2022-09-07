4d9058d8fe2c248ce7e3f1f77309b4869ddf144c
Erjan <erjan@users.noreply.github.com>
2022-09-07 20:24:13 +0500

    Update flatten_binary_tree_to_linked_list.py

--- flatten_binary_tree_to_linked_list.py
+++ flatten_binary_tree_to_linked_list.py
@@ -50,4 +50,34 @@ def flatten(self, root):
             node1.right = node2
         root = root.right
             
+----------------------------------------------------------------------------------------------
+class Solution:
+      
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Input: root node of binary tree
+        Output: convert binary tree to right-skewed linked list
+        """
         
+        # record of node of previous traversal
+        previous_traversal = None
+        
+        def helper( node):
+        
+            if node:
+
+                # DFS travesal to next level
+                
+                helper( node.right )
+                helper( node.left )
+
+                # flattern binary tree to right skewed linked list
+                
+                nonlocal previous_traversal
+                node.right = previous_traversal
+                node.left = None
+                previous_traversal = node
+                
+        # ---------------------
+        
+        helper(root)
