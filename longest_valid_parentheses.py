9ee8353d52f19cf77413dafc2ad2b70786b6f68b
Erjan <erjan@users.noreply.github.com>
2022-09-09 07:10:20 +0500

    Update longest_valid_parentheses.py

--- longest_valid_parentheses.py
+++ longest_valid_parentheses.py
@@ -22,3 +22,55 @@ class Solution:
                 else:
                     res = max(res, i - stack[-1])
         return res
+------------------------------------------------------------
+
+from time import sleep
+
+TIME = 0.9
+
+
+def f(s):
+
+    res = 0
+    sleep(TIME)
+    stack = list()
+    stack.append(-1)
+
+    for i in range(len(s)):
+        print()
+        print()
+        print(
+            '----------------index %d, cur char: %s-------------------------' % (i, s[i]))
+        sleep(TIME)
+
+        if s[i] == '(':
+            sleep(TIME)
+            print('found (')
+            stack.append(i)
+        else:
+            sleep(TIME)
+            print('popping stack')
+            stack.pop()
+            print('stack now: ')
+            print(stack)
+
+            if len(stack) == 0:
+                sleep(TIME)
+                print('stack empty add i: %d' % i)
+                stack.append(i)
+
+            else:
+                sleep(TIME)
+                res = max(res, i - stack[-1])
+                print('updating res, res now: %d ' % res)
+    print()
+    print('res %d ' % res)
+    return res
+
+
+if __name__ == "__main__":
+
+    s = ")()())))(()()()()()()()"
+        #0123456789
+
+    f(s)
