6dee849b8274935af9d7cae72a6ecba1701cceb8
Erjan <erjan@users.noreply.github.com>
2022-09-09 15:25:22 +0500

    Update first_missing_positive.py

--- first_missing_positive.py
+++ first_missing_positive.py
@@ -27,4 +27,36 @@ class Solution:
                 return i
         return len(nums)+1
         
+--------------------------------------------------------------------------------------
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        
+        has1 = False
+        for x in nums:
+            if x == 1:
+                has1 = True
+                break
+        if not has1:
+            return 1
+        
+        n = len(nums)
+        
+        if n == 1:
+            return 2
+        
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n :
+                nums[i] = 1
+        
+        for i in range(n):
+            x = abs(nums[i])
+            if nums[x-1] > 0:
+                nums[x-1] *= -1
+        
+        for i in range(n):
+            if nums[i] >0:
+                return i+1
+        
+        return n+1
+        
         
