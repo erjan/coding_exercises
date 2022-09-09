e30f076add4b4c07c30aa4ffa2f4b33759b36db2
Erjan <erjan@users.noreply.github.com>
2022-09-09 16:18:39 +0500

    Update trapping_rain_water.py

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -26,5 +26,25 @@ class Solution:
         
         return res
                 
+----------------------------
+#stack based
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        ans = 0
+        stack = []
+        
+        for i in range(n):
+            l = len(stack)
+            if l == 0 or height[i] < height[stack[l - 1]]:
+                stack.append(i)
+            else:
+                while(l > 0 and height[stack[l - 1]] <= height[i]):
+                    ht = height[stack.pop()]
+                    l = l - 1
+                    ans = ans + (0 if l == 0 else (min(height[i],height[stack[l - 1]]) - ht) * (i - stack[l - 1] - 1))
+                stack.append(i)
+        return ans
                 
         
