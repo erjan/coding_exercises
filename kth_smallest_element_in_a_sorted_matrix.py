d4eb2f70de9cbff2d9ae16feec974e040252a898
Erjan <erjan@users.noreply.github.com>
2022-09-09 21:48:24 +0500

    Update kth_smallest_element_in_a_sorted_matrix.py

--- kth_smallest_element_in_a_sorted_matrix.py
+++ kth_smallest_element_in_a_sorted_matrix.py
@@ -17,3 +17,25 @@ class Solution:
         tmp.sort()
         
         return tmp[k-1]
+-----------------------------------------------------------------------
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        
+        l = matrix[0][0]
+        r = matrix[-1][-1]
+        N = len(matrix)
+                
+        def less_k(m):
+            cnt = 0
+            for i in range(N):
+                x = bisect_right(matrix[i],m)
+                cnt+=x
+            return cnt
+        
+        while l < r:
+            mid = (l+r)//2
+            if less_k(mid) < k:
+                l = mid+1
+            else:
+                r= mid
+        return l
