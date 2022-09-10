c0068b4761d67b7be3f241aae8ceae21a7dc00a2
Erjan <erjan@users.noreply.github.com>
2022-09-10 20:09:31 +0500

    Create search_2d_matrix_ii.py

---
+++ search_2d_matrix_ii.py
@@ -0,0 +1,40 @@
+'''
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+'''
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        if not len(matrix) or not len(matrix[0]):
+			# Quick response for empty matrix
+            return False
+        
+        h, w = len(matrix), len(matrix[0])
+        
+        # Start adaptive search from bottom left corner
+        y, x = h-1, 0
+        
+        while True:
+            
+            if y < 0 or x >= w:
+                break
+            
+            current = matrix[y][x]
+            
+            if target < current:
+                # target is smaller, then go up
+                y -= 1
+            
+            elif target > current:
+                # target is larger, then go right
+                x += 1
+            
+            else:
+                # hit target
+                return True
+            
+        return False
