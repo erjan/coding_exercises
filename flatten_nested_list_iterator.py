82700d6e9dd40f39c5ad62813fe438d4e08e77ab
Erjan <erjan@users.noreply.github.com>
2022-09-10 22:34:04 +0500

    Update flatten_nested_list_iterator.py

--- flatten_nested_list_iterator.py
+++ flatten_nested_list_iterator.py
@@ -37,3 +37,37 @@ class NestedIterator:
                 s.append([x.getList(),0])
                 
         return False
+--------------------------------------------------------------------
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.data = []
+        self.flatten(nestedList)
+        
+    def flatten(self, lst):
+        for el in lst:
+            if el.isInteger(): self.data.append(el.getInteger())
+            else: self.flatten(el.getList())
+    
+    def hasNext(self) -> bool: return len(self.data)
+    
+    def next(self) -> int: return self.data.pop(0)
+    
+----------------------------------------------
+def dfs(nested, flat):
+    for elem in nested:
+        if elem.isInteger():
+            flat.append(elem.getInteger())
+        else:
+            dfs(elem.getList(), flat)
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.flatList, self.i = [], 0
+        dfs(nestedList, self.flatList)
+    
+    def next(self) -> int:
+        self.i += 1
+        return self.flatList[self.i-1]
+    
+    def hasNext(self) -> bool:
+        return self.i < len(self.flatList)
