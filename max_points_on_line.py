578d7378ca0b821b8ba8ff729f1b3ad0288665a8
Erjan <erjan@users.noreply.github.com>
2022-09-11 20:52:43 +0500

    Create max_points_on_line.py

---
+++ max_points_on_line.py
@@ -0,0 +1,25 @@
+'''
+Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+'''
+
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) <= 2:
+            return len(points)
+        
+        def find_slope(p1, p2):
+            x1, y1 = p1
+            x2, y2 = p2
+            if x1-x2 == 0:
+                return inf
+            return (y1-y2)/(x1-x2)
+        
+        ans = 1
+        for i, p1 in enumerate(points):
+            slopes = defaultdict(int)
+            for j, p2 in enumerate(points[i+1:]):
+                slope = find_slope(p1, p2)
+                slopes[slope] += 1
+                ans = max(slopes[slope], ans)
+        return ans+1
