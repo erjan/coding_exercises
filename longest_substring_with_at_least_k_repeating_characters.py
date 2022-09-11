0577abe5239860cacaa65c304d50bee438de7f23
Erjan <erjan@users.noreply.github.com>
2022-09-11 19:09:19 +0500

    Update longest_substring_with_at_least_k_repeating_characters.py

--- longest_substring_with_at_least_k_repeating_characters.py
+++ longest_substring_with_at_least_k_repeating_characters.py
@@ -50,3 +50,24 @@ class Solution:
         # the length of s is the answer exactly
         
         return len(s)
+-----------------------------------------------------------------------------------------------
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        problematic_letters = []
+        valid = True
+        counter = Counter(s)
+        for letter in counter:
+            if counter[letter] < k:
+                problematic_letters.append(letter)
+                valid = False
+        if valid:
+            return len(s)
+
+        for letter in problematic_letters:
+            s = s.replace(letter, ' ')
+        strings_after_divide = s.split()
+
+        ans = 0
+        for string in strings_after_divide:
+            ans = max(ans, self.longestSubstring(string, k))
+        return ans
