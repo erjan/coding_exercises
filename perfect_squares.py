e671b73fdbda8546f49feca1c78badb916a91637
Erjan <erjan@users.noreply.github.com>
2022-09-11 12:03:12 +0500

    Update perfect_squares.py

--- perfect_squares.py
+++ perfect_squares.py
@@ -6,6 +6,20 @@ that is the square of an integer; in other words, it is the product of some inte
 with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
 '''
 
+#BFS
+from collections import deque
+class Solution:
+    def numSquares(self, n: int) -> int:
+        queue = deque([(n, 0)])
+        while queue:
+            cur_n, cur_steps = queue.popleft()
+            if cur_n**0.5 == int(cur_n**0.5):
+                return cur_steps+1
+            for i in range(1, int(cur_n**0.5)+1):
+                queue.append((cur_n-i*i, cur_steps+1))
+		
+------------------------------------------------------		
+#DP
 class Solution:
     def numSquares(self, n: int) -> int:
         dp = [float("inf")]*(n+1)
