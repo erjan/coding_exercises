bae163d1e7ff870299af18e954a2d4f7252ab3c7
Erjan <erjan@users.noreply.github.com>
2022-09-12 13:53:08 +0500

    Update random_pick_index.py

--- random_pick_index.py
+++ random_pick_index.py
@@ -86,3 +86,33 @@ class Solution:
             elif random.random() < 1 / count:
                 chosen_index = i
         return chosen_index
+
+---------------------------------------------------------------------------------------
+class Solution {
+
+    private int[] nums;
+    
+    private Random rand;
+    
+    public Solution(int[] nums) {
+        // Do not allocate extra space for the nums array
+        this.nums = nums;
+        this.rand = new Random();
+    }
+    
+    public int pick(int target) {
+        List<Integer> indices = new ArrayList<>();
+        int n = this.nums.length;
+        int count = 0;
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
+            if (this.nums[i] == target) {
+                indices.add(i);
+            }
+        }
+        int l = indices.size();
+        // pick an index at random
+        int randomIndex = indices.get(rand.nextInt(l));
+        return randomIndex;
+    }
+}
