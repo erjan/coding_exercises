1b90adb5d9f0a28314cd880da69ca497b3baf4d0
Erjan <erjan@users.noreply.github.com>
2022-09-12 16:18:43 +0500

    Update minimum_interval_to_include_each_query.py

--- minimum_interval_to_include_each_query.py
+++ minimum_interval_to_include_each_query.py
@@ -66,3 +66,25 @@ class Solution:
                 ans[queries.pop(idx)[1]] = right - left + 1
         return ans
 	
+---------------------------------------------------------------------------------------------------------
+
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        intervals.sort(key=lambda i:i[0])
+        result = {}
+        heap = []
+        i = 0
+        for q in sorted(queries):
+            # add the intervals into heap for the given query
+            while i < len(intervals) and intervals[i][0] <= q:
+                interval_start,interval_end = intervals[i] 
+                heapq.heappush(heap,(interval_end-interval_start+1,interval_end))
+                i+=1
+            # pop out the invalid intervals from the heap against which the given query will not overlap the interval
+            while heap and heap[0][1] < q:
+                heapq.heappop(heap)
+            # after the while loop ends we will at all potential intervals that satisfy the query
+            # take the min if heap in not null otherwise -1
+            min_val = heap[0][0] if heap else -1
+            result[q] = min_val
+        return [result[q] for q in queries]
