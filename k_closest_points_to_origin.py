5b8f43b045ffd5d76d178c064b368bec739fdcae
Erjan <erjan@users.noreply.github.com>
2022-09-12 21:40:42 +0500

    Update k_closest_points_to_origin.py

--- k_closest_points_to_origin.py
+++ k_closest_points_to_origin.py
@@ -19,3 +19,41 @@ class Solution:
                 heapq.heappush(heap, (dist, x, y))
         
         return [(x,y) for (dist,x, y) in heap]
+---------------------------------------------------------------------------------------
+Standard QuickSelect Algorithm(like QuickSort)
+Given a sequence seq and a positive number k, we need to find out the top k elements from seq.
+Randomly select a pivot from the current seq
+random selection is very important, on average, this can effectively deal with the worst case.
+Traverse seq one by one, count how many elements less than or greater than or equal to the pivot respectively.
+There are 3 different situations
+k <= len(less)
+that means you can find all top k elements in less recursively.
+len(less) < k <= len(less) + len(equal)
+congratulations! pivot is the k-th element, note to handle equal situations carefully.
+k > len(less) + len(equal)
+Not bad. you have already found the top len(less) + len(equal) elements.
+you can find out the left k - len(less) + len(equal) top elements from greater recursively.
+
+
+class Solution(object):
+    def kClosest(self, points, k):
+        def quickSelect(points, k):
+            less, equal, greater = [], [], []
+            pivot = random.choice(points)
+            pivot_l2 = pivot[0] ** 2 + pivot[1] ** 2
+            for p in points:
+                p_l2 = p[0] ** 2 + p[1] ** 2
+                if p_l2 < pivot_l2:
+                    less.append(p)
+                elif p_l2 > pivot_l2:
+                    greater.append(p)
+                else:
+                    equal.append(p)
+            if len(less) < k <= len(less) + len(equal):
+                return less + equal[:k - len(less)]
+            elif k <= len(less):
+                return quickSelect(less, k)
+            else:
+                return less + equal + quickSelect(greater, k - len(less) - len(equal))
+				
+        return quickSelect(points, k)
