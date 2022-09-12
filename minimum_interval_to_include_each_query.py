8930e5be04b3ccbef363879c6f7b9f1ca6c54599
Erjan <erjan@users.noreply.github.com>
2022-09-12 16:17:25 +0500

    Update minimum_interval_to_include_each_query.py

--- minimum_interval_to_include_each_query.py
+++ minimum_interval_to_include_each_query.py
@@ -53,3 +53,16 @@ class Solution:
 			hashMap[q] = minHeap[0][0] if minHeap else -1
 
 		return [hashMap[q] for q in queries]
+	
+-------------------------------------------------------------------------------------------
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        ans = [-1] * len(queries)
+        intervals.sort(key = lambda i: i[1] - i[0])
+        queries = sorted([q, i] for i, q in enumerate(queries))
+        for left, right in intervals:
+            idx = bisect.bisect(queries, [left])
+            while idx < len(queries) and queries[idx][0] <= right:
+                ans[queries.pop(idx)[1]] = right - left + 1
+        return ans
+	
