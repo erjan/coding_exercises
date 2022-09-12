d947885bc6a8ea60661976f6472537fc190e6d01
Erjan <erjan@users.noreply.github.com>
2022-09-12 14:18:36 +0500

    Update min_cost_to_connect_all_points.py

--- min_cost_to_connect_all_points.py
+++ min_cost_to_connect_all_points.py
@@ -60,3 +60,29 @@ class Solution:
             cost += w
         
         return cost
+----------------------------------------------------------------------------------
+def minCostConnectPoints(self, points: List[List[int]]) -> int:
+    #prims
+    g = defaultdict(list)
+    n = len(points)
+	#create graph
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                g[i].append((abs(points[j][0]-points[i][0])+abs(points[j][1]-points[i][1]), j))
+    
+    heap = [(0,0)]
+    ans = 0
+    visited = set()
+    while heap:
+        weight, to = heapq.heappop(heap)
+        if to in visited:
+            continue
+        ans += weight
+        visited.add(to)
+        
+        for cost, nei in g[to]:
+            if nei not in visited:
+                heapq.heappush(heap, (cost, nei))
+    
+    return ans
