8ff147e260c64e64c40cbe9e7329ea2cfa19051c
Erjan <erjan@users.noreply.github.com>
2022-09-13 10:26:55 +0500

    Update longest_duplicate_substring.py

--- longest_duplicate_substring.py
+++ longest_duplicate_substring.py
@@ -26,3 +26,16 @@ class Solution:
                 j += 1
                 window = s[i:i+j]
         return ans
+-----------------------------------------------------------------------------------------------
+class Solution:
+    def longestDupSubstring(self, s: str) -> str:
+        ans = ''
+        j   = 1
+        for i in range(len(s)):
+            longest = s[i:i+j]
+            temp    = s[i+1:]
+            while longest in temp:
+                ans = longest
+                j += 1
+                longest = s[i:i+j]
+        return ans
