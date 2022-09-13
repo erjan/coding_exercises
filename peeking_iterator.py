7a9ba7d2abb88bff6c37c8a9728e824e7f1f1558
Erjan <erjan@users.noreply.github.com>
2022-09-13 13:47:19 +0500

    Update peeking_iterator.py

--- peeking_iterator.py
+++ peeking_iterator.py
@@ -45,3 +45,35 @@ class PeekingIterator:
 
 	def hasNext(self):
 		return self.helper != None
+-------------------------------------------------------------------------------------------------
+class PeekingIterator:
+    def __init__(self, iterator):
+        """
+        Initialize your data structure here.
+        :type iterator: Iterator
+        """
+        self.iterator = iterator
+        self.to_peek = self.iterator.next() if self.iterator.hasNext() else None
+
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        return self.to_peek
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        temp = self.to_peek
+        self.to_peek = self.iterator.next() if self.iterator.hasNext() else None
+        return temp
+
+        
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self.to_peek is not None
