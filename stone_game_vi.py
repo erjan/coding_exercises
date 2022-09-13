817cdfca46369af789d5f8ba69f9bf15ed0f788b
Erjan <erjan@users.noreply.github.com>
2022-09-13 21:16:54 +0500

    Update stone_game_vi.py

--- stone_game_vi.py
+++ stone_game_vi.py
@@ -31,3 +31,25 @@ class Solution:
 ------------------------------------------------------------------------
  def stoneGameVI(self, A, B):
         return cmp(-sum(B) + sum(sorted(a + b for a,b in zip(A,B))[::-2]), 0)
+    
+----------------------------------------------------------------------------------------
+Intuition
+Sort stones by their sum value for Alice and Bob.
+If a stone is super valued for Alice, Alice wants to take it.
+If a stone is super valued for Bob, Alice also wants to take it.
+Because she doesn't want Bob to take it.
+
+
+Explanation
+Here is more convinced explanation.
+Assume a stone valued [a,b] for Alice and Bod.
+Alice takes it, worth a for Alice,
+Bob takes it, worth b for Bob,
+we can also consider that it worth -b for Alice.
+The difference will be a+b.
+That's the reason why we need to sort based on a+b.
+And Alice and Bob will take one most valued stone each turn.
+----------------------------------------------------------------------
+ def stoneGameVI(self, A, B):
+        A = sorted(zip(A, B), key=sum)
+        return cmp(sum(a for a, b in A[::-2]), sum(b for a, b in A[-2::-2]))
