d9d962b81b6e1e7fbf7a865c8022b1985ef4ca5a
Erjan <erjan@users.noreply.github.com>
2022-09-13 22:04:47 +0500

    Update stone_game_iii.py

--- stone_game_iii.py
+++ stone_game_iii.py
@@ -103,4 +103,21 @@ def stoneGameIII(self, stoneValue):
     if s > 0: return 'Alice'
     if s == 0: return 'Tie'
     if s < 0: return 'Bob'
+    
+--------------------------------------------------------------------------------------
+class Solution:
+    def stoneGameIII(self, sv: List[int]) -> str:
+        s=0
+        dp=[0 for _ in range(50003)]
+        for i in range(len(sv)-1,-1,-1):
+            dp[i]=-float('inf')
+            s+=sv[i]
+            for j in range(1,4):
+                dp[i]=max(dp[i],s-dp[i+j])
+        if s-dp[0]==dp[0]:
+            return 'Tie'
+        elif s-dp[0]>dp[0]:
+            return 'Bob'
+        else:
+            return 'Alice'
         
