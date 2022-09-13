20aa5e62cd48d111451c69b702d2e901507ff066
Erjan <erjan@users.noreply.github.com>
2022-09-13 16:32:24 +0500

    Create predict_the_winner.py

---
+++ predict_the_winner.py
@@ -0,0 +1,7 @@
+'''
+You are given an integer array nums. Two players are playing a game with this array: player 1 and player 2.
+
+Player 1 and player 2 take turns, with player 1 starting first. Both players start the game with a score of 0. At each turn, the player takes one of the numbers from either end of the array (i.e., nums[0] or nums[nums.length - 1]) which reduces the size of the array by 1. The player adds the chosen number to their score. The game ends when there are no more elements in the array.
+
+Return true if Player 1 can win the game. If the scores of both players are equal, then player 1 is still the winner, and you should also return true. You may assume that both players are playing optimally.
+'''
