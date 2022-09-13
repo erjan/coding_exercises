f35e5e4b7cfaa4bf40cc2366bbee31a49221c4f0
Erjan <erjan@users.noreply.github.com>
2022-09-13 15:26:50 +0500

    Update reverse_pairs.py

--- reverse_pairs.py
+++ reverse_pairs.py
@@ -57,3 +57,46 @@ class Solution:
             rv += ft.sum(numsRanks[i]-1)
             ft.add(doubleNumsRanks[i], 1)
         return rv
+
+---------------------------------------------------------------------------------------------------------
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        def merge(low,mid,high):
+            cnt=0
+            j=mid+1
+            for i in range(low,mid+1):
+                while j<=high and nums[i]>2*(nums[j]):
+                    j+=1
+                cnt+=j-(mid+1)
+            temp=[]
+            left=low
+            right=mid+1
+            while left<=mid and right<=high:
+                if nums[left]<=nums[right]:
+                    temp.append(nums[left])
+                    left+=1
+                else:
+                    temp.append(nums[right])
+                    right+=1    
+                    
+            while left<=mid:
+                temp.append(nums[left])
+                left+=1
+            while right<=high:
+                temp.append(nums[right])
+                right+=1
+                
+            for i in range(low,high+1):
+                nums[i]=temp[i-low]
+            return cnt
+        def mergeSort(low,high):
+            if low>=high:
+                return 0
+            mid=(low+high)//2
+            inv=mergeSort(low,mid)
+            inv+=mergeSort(mid+1,high)
+            inv+=merge(low,mid,high)
+            return inv
+        
+        return mergeSort(0,len(nums)-1)
+    
