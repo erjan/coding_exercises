7dd367ffb4f6411d2bcccdcc5bbe4444ccce04f5
Erjan <erjan@users.noreply.github.com>
2022-09-14 15:35:55 +0500

    Update check_if_word_can_be_placed_in_crossword.py

--- check_if_word_can_be_placed_in_crossword.py
+++ check_if_word_can_be_placed_in_crossword.py
@@ -85,4 +85,19 @@ class Solution:
                 return True
         
         return False
+
+--------------------------------------------------------
+class Solution:
+    def placeWordInCrossword(self, board: List[List[str]], word: str) -> bool:
+        words=[word,word[::-1]]
+        n=len(word)
+        for B in board,zip(*board):
+            for row in B:
+                q=''.join(row).split('#')
+                for w in words:
+                    for s in q:
+                        if len(s)==n:
+                            if all(s[i]==w[i] or s[i]==' ' for i in range(n)):
+                                return True
+        return False
       
