8a957a0e496ed01822c8f018d05efdbb18df7437
Erjan <erjan@users.noreply.github.com>
2022-09-14 15:16:18 +0500

    Update minimum_cost_to_set_cooking_time.py

--- minimum_cost_to_set_cooking_time.py
+++ minimum_cost_to_set_cooking_time.py
@@ -38,3 +38,22 @@ class Solution:
                 if m * 60 + s == targetSeconds: 
                     ans = min(ans, count_cost(m, s))
         return ans
+
+----------------------------------------------------------------------------------------------------------------------------------
+class Solution:
+    def minCostSetTime(self, startAt: int, moveCost: int, pushCost: int, targetSeconds: int) -> int:
+        def cost(mins, secs):
+            s, curr, res = str(mins * 100 + secs), str(startAt), 0
+            for ch in s:
+                if ch == curr: res += pushCost
+                else:
+                    res += (pushCost + moveCost)
+                    curr = ch
+            return res
+
+        maxmins, ans = targetSeconds // 60, float('inf')
+        for mins in range(maxmins + 1):
+            secs = targetSeconds - mins * 60
+            if secs > 99 or mins > 99: continue
+            ans = min(ans, cost(mins, secs))
+        return ans        
