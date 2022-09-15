b5e5803c54b5f0e6f34ba4decf616f911b59d43d
Erjan <erjan@users.noreply.github.com>
2022-09-15 15:58:02 +0500

    Create valid_number.py

---
+++ valid_number.py
@@ -0,0 +1,29 @@
+'''
+A valid number can be split up into these components (in order):
+
+A decimal number or an integer.
+(Optional) An 'e' or 'E', followed by an integer.
+A decimal number can be split up into these components (in order):
+
+(Optional) A sign character (either '+' or '-').
+One of the following formats:
+One or more digits, followed by a dot '.'.
+One or more digits, followed by a dot '.', followed by one or more digits.
+A dot '.', followed by one or more digits.
+An integer can be split up into these components (in order):
+
+(Optional) A sign character (either '+' or '-').
+One or more digits.
+For example, all the following are valid numbers: ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"], while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
+'''
+
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        if "inf" in s or "Inf" in s:
+            return False
+        try:
+            complex(s) # for int, long, float and complex
+        except ValueError:
+            return False
+        
+        return True
