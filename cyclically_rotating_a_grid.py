c98a8ed0da9359dfdcde23ba7d2a33fcd3c49ced
Erjan <erjan@users.noreply.github.com>
2022-09-16 22:41:19 +0500

    Create cyclically_rotating_a_grid.py

---
+++ cyclically_rotating_a_grid.py
@@ -0,0 +1,44 @@
+'''
+You are given an m x n integer matrix grid​​​, where m and n are both even integers, and an integer k.
+
+The matrix is composed of several layers, which is shown in the below image, where each color is its own layer:
+'''
+
+class Solution:
+    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        n = len(grid)
+        m = len(grid[0])
+        
+        i, j = 0, 0
+        bottom, right = n-1, m-1 
+        while i < n//2 and j < m//2:
+            temp = []
+            for x in range(j, right):
+                temp.append(grid[i][x])
+            for x in range(i, bottom):
+                temp.append(grid[x][right])
+            for x in range(right, j, -1):
+                temp.append(grid[bottom][x])
+            for x in range(bottom, i, -1):
+                temp.append(grid[x][j])
+                
+            
+            indx = 0
+            for x in range(j, right):
+                grid[i][x] = temp[(k + indx)%len(temp)]
+                indx += 1
+            for x in range(i, bottom):
+                grid[x][right] = temp[(k + indx)%len(temp)]
+                indx += 1
+            for x in range(right, j, -1):
+                grid[bottom][x] = temp[(k + indx)%len(temp)]
+                indx += 1
+            for x in range(bottom, i, -1):
+                grid[x][j] = temp[(k + indx)%len(temp)]
+                indx += 1
+                
+            i += 1
+            j += 1
+            bottom -= 1
+            right -= 1
+        return grid
