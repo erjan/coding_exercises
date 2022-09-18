679b6b258371e73ca0d665508e772ea51af27542
Erjan <erjan@users.noreply.github.com>
2022-09-19 03:40:14 +0500

    Create number_of_people_aware_of_secret.py

---
+++ number_of_people_aware_of_secret.py
@@ -0,0 +1,15 @@
+'''
+On day 1, one person discovers a secret.
+
+You are given an integer delay, which means that each person will share the secret with a new person every day, starting from delay days after discovering the secret. You are also given an integer forget, which means that each person will forget the secret forget days after discovering it. A person cannot share the secret on the same day they forgot it, or on any day afterwards.
+
+Given an integer n, return the number of people who know the secret at the end of day n. Since the answer may be very large, return it modulo 109 + 7.
+'''
+
+    def peopleAwareOfSecret(self, n, delay, forget):
+        dp = [1] + [0] * (n - 1)
+        mod = 10 ** 9 + 7
+        share = 0
+        for i in range(1, n):
+            dp[i] = share = (share + dp[i - delay] - dp[i - forget]) % mod
+        return sum(dp[-forget:]) % mod
