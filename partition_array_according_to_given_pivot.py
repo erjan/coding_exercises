3260e485539255055c51b15e8ac07fab61de3b88
Erjan <erjan@users.noreply.github.com>
2022-09-19 21:30:20 +0500

    Create partition_array_according_to_given_pivot.py

---
+++ partition_array_according_to_given_pivot.py
@@ -0,0 +1,23 @@
+'''
+You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+
+Every element less than pivot appears before every element greater than pivot.
+Every element equal to pivot appears in between the elements less than and greater than pivot.
+The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+More formally, consider every pi, pj where pi is the new position of the ith element and pj is the new position of the jth element. For elements less than pivot, if i < j and nums[i] < pivot and nums[j] < pivot, then pi < pj. Similarly for elements greater than pivot, if i < j and nums[i] > pivot and nums[j] > pivot, then pi < pj.
+Return nums after the rearrangement.
+'''
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        
+        
+        before = list(filter(lambda x: x < pivot, nums))
+
+        equal = list(filter(lambda x: x == pivot, nums))
+        after = list(filter(lambda x: x > pivot, nums))
+
+
+        nums = before + equal + after
+        print(nums)
+        return nums
