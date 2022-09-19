58916442426ac20b53970983af1346eb7cac60dc
Erjan <erjan@users.noreply.github.com>
2022-09-19 21:33:05 +0500

    Update partition_array_according_to_given_pivot.py

--- partition_array_according_to_given_pivot.py
+++ partition_array_according_to_given_pivot.py
@@ -21,3 +21,26 @@ class Solution:
         nums = before + equal + after
         print(nums)
         return nums
+
+    
+--------------------------------------------------------------------------------------------------
+class Solution:
+	def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+
+		ans=[]
+
+		nums.remove(pivot)
+
+		i=0
+		ans.append(pivot)
+
+		for j in nums:
+			if j<pivot:
+				ans.insert(i,j)
+				i=i+1
+			elif j==pivot:
+				ans.insert(i+1,j)
+			else:
+				ans.append(j)
+
+		return ans
