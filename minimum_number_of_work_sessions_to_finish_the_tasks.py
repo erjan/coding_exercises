d663400e4c9e8f11d1d8392eae9338e66c0b8282
Erjan <erjan@users.noreply.github.com>
2022-09-20 19:02:53 +0500

    Update minimum_number_of_work_sessions_to_finish_the_tasks.py

--- minimum_number_of_work_sessions_to_finish_the_tasks.py
+++ minimum_number_of_work_sessions_to_finish_the_tasks.py
@@ -45,3 +45,64 @@ class Solution:
                 l = m + 1
 
         return l
+    
+------------------------------------------------------------
+#bottom up dp
+class Solution:
+    def minSessions(self, tasks: List[int], sessionTime: int) -> int:
+        
+        dp = [set() for _ in range(len(tasks))]
+        
+        dp[0].add(tuple([sessionTime - tasks[0]]))
+        
+        for i in range(1, len(tasks)):
+            for prev_session in dp[i-1]:
+                dp[i].add(tuple(sorted(list(prev_session) + [sessionTime - tasks[i]])))
+                
+                for j in range(0, len(prev_session)):
+                    if prev_session[j] >= tasks[i]:
+                        next_session = list(prev_session) + []
+                        next_session[j] -= tasks[i]
+                        dp[i].add(tuple(sorted(next_session)))
+                        
+        ans = len(tasks)
+        for x in dp[len(tasks) - 1]:
+            ans = min(ans, len(x))
+        
+        return ans
+-----------------------------------------------------------------------------
+#top down dp
+
+class Solution:
+    def minSessions(self, tasks: List[int], sessionTime: int) -> int:
+        
+        @cache
+        def dp(mask_tuple, prev):
+            
+            if len(mask_tuple) == 0:
+                return 1
+            
+            
+            mask_list = list(mask_tuple)
+            ans = inf
+            
+            for i in range(len(mask_list)):
+                
+                new_tuple = mask_list[:i] + mask_list[i+1:] 
+                curr = prev + mask_list[i]
+                temp = inf
+                
+                if curr > sessionTime:
+                    # cant take this task. start new session
+                    # thiis can lead to inf loop
+                    temp = 1 + dp(mask_tuple, 0)
+                else:
+                    # can include this task in same session
+                    temp = dp(tuple(new_tuple), curr)
+                
+                ans = min(ans, temp)
+            
+            return ans
+                    
+        return dp(tuple(tasks), 0)
+    
