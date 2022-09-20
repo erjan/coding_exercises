0c886dfb6fe971ae04d0733e444c80bfe5836ac4
Erjan <erjan@users.noreply.github.com>
2022-09-20 22:46:12 +0500

    Create matchsticks_to_square.py

---
+++ matchsticks_to_square.py
@@ -0,0 +1,30 @@
+'''
+You are given an integer array matchsticks where matchsticks[i] is the length of 
+the ith matchstick. You want to use all the matchsticks to make one square. You should not break any 
+stick, but you can link them up, and each matchstick must be used exactly one time.
+
+Return true if you can make this square and false otherwise.
+
+'''
+
+
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        value = sum(matchsticks)
+        if value < 4:
+            return False
+        if value % 4 != 0:
+            return False
+        edge = value // 4
+        matchsticks.sort(reverse=True)
+        @cache
+        def findedges(l1, l2, l3, l4, i):
+            nonlocal edge
+            if l1 == l2 == l3 == l4 == edge:
+                return True
+            if i > len(matchsticks) - 1:
+                return False
+            if l1 > edge or l2 > edge or l3 > edge or l4 > edge:
+                return False
+            return findedges(l1 + matchsticks[i], l2, l3, l4, i + 1) or findedges(l1, l2 + matchsticks[i] , l3, l4, i + 1) or findedges(l1, l2, l3 + matchsticks[i], l4, i + 1) or findedges(l1, l2, l3, l4 + matchsticks[i] , i + 1)
+        return findedges(0, 0, 0, 0, 0)
