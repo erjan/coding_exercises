51d0ad0d0c19f6d5e8f8ba66285fa4a0f8f4a853
Erjan <erjan@users.noreply.github.com>
2022-09-25 03:33:08 +0500

    Update maximum_length_of_a_concatenated_string_with_unique_characters.py

--- maximum_length_of_a_concatenated_string_with_unique_characters.py
+++ maximum_length_of_a_concatenated_string_with_unique_characters.py
@@ -10,13 +10,13 @@ class Solution:
     def maxLength(self, arr: List[str]) -> int:
         self.maximum = 0
         
-    def backtrack(start,array,s):
-        if len(s)==len(set(s)):
-            self.maximum = max(self.maximum,len(s))
-        else:
-            return
-        for i in range(start,len(array)):
-            backtrack(i+1,array,s + array[i])
-            
-    backtrack(0,arr,"")
-    return self.maximum
+        def backtrack(start,array,s):
+            if len(s)==len(set(s)): #basically check if the generated accumulated string is unique
+                self.maximum = max(self.maximum,len(s))
+            else:
+                return
+            for i in range(start,len(array)):
+                backtrack(i+1,array,s + array[i])
+
+        backtrack(0,arr,"")
+        return self.maximum
