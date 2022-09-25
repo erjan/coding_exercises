98c498b026a1d0e22ec13717ce63bd4f90d731cc
Erjan <erjan@users.noreply.github.com>
2022-09-26 00:56:09 +0500

    Update recover_binary_search_tree.py

--- recover_binary_search_tree.py
+++ recover_binary_search_tree.py
@@ -25,3 +25,22 @@ class Solution:
             if currNode != targetNode:
                 currNode.val, targetNode.val = targetNode.val, currNode.val
                 break
+
+                
+--------------------------------------------------------------------------------------------------
+def recoverTree(self, root: Optional[TreeNode]) -> None:
+        in_order = []
+        def inOrder(node):
+            if node is None:
+                return
+            inOrder(node.left)
+            in_order.append(node)
+            inOrder(node.right)
+        
+        inOrder(root)
+
+        sorted_order = sorted(in_order, key=lambda x:x.val)
+        for i in range(len(in_order)):
+            if in_order[i] != sorted_order[i]:
+                in_order[i].val, sorted_order[i].val = sorted_order[i].val, in_order[i].val
+                return
