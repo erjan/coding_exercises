9d60d1e69ed6869df4e33b7137ee165f0fb3f666
Erjan <erjan@users.noreply.github.com>
2022-09-25 05:00:40 +0500

    Update trie_template.py

--- trie_template.py
+++ trie_template.py
@@ -1,17 +1,17 @@
 class TrieNode():
- def __init__(self):
-    self.child = defaultdict(TrieNode)
+	def __init__(self):
+    		self.child = defaultdict(TrieNode)
 
 class Trie():
-def __init__(self):
-    self.root = TrieNode()
-    self.list = []
+	def __init__(self):
+    		self.root = TrieNode()
+    		self.list = []
 
 	def add(self, word):
-    node = self.root
-    for ch in word:
-        node = node.child[ch]
-    self.list.append((node, len(word)+1))
+	    node = self.root
+	    for ch in word:
+		node = node.child[ch]
+	    self.list.append((node, len(word)+1))
 
 
  class Solution(object):
