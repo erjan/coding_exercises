d7087c3fd7945ec0bcd8e6fea2854659d005a66d
Erjan <erjan@users.noreply.github.com>
2022-09-27 12:29:03 +0500

    Create transform_to_chessboard.py

---
+++ transform_to_chessboard.py
@@ -0,0 +1,25 @@
+'''
+You are given an n x n binary grid board. In each move, you can swap any two rows with each other, or any two columns with each other.
+
+Return the minimum number of moves to transform the board into a chessboard board. If the task is impossible, return -1.
+
+A chessboard board is a board where no 0's and no 1's are 4-directionally adjacent.
+'''
+
+class Solution:
+    def movesToChessboard(self, board: List[List[int]]) -> int:
+        n=len(board)
+        if n<=1:
+            return 0
+        rows=[''.join(str(c) for c in r) for r in board]
+        counter=collections.Counter(rows)
+        keys=list(counter)
+        if len(keys)!=2 or abs(counter[keys[0]]-counter[keys[1]])>1 \
+            or abs(keys[0].count('1')-keys[0].count('0'))>1 \
+            or any(a==b for a,b in zip(*keys)):
+            return -1
+        rowdiff=sum(board[0][i]!=(i%2) for i in range(n))
+        coldiff=sum(board[i][0]!=(i%2) for i in range(n))
+        rowdiff=n-rowdiff if rowdiff%2!=0 or (n%2==0 and (n-rowdiff)<rowdiff) else rowdiff
+        coldiff=n-coldiff if coldiff%2!=0 or (n%2==0 and (n-coldiff)<coldiff) else coldiff
+        return (rowdiff+coldiff)//2
