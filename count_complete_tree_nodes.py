0d7382b4bccfffe4b2551255d310f53e9958d743
Erjan <erjan@users.noreply.github.com>
2022-10-01 17:44:30 +0500

    Update count_complete_tree_nodes.py

--- count_complete_tree_nodes.py
+++ count_complete_tree_nodes.py
@@ -9,6 +9,52 @@ compare the depth between left sub tree and right sub tree.
 A, If it is equal, it means the left sub tree is a full binary tree
 B, It it is not , it means the right sub tree is a full binary tree
 
+
+''' 
+Only traversing along the left and right boundary. 
+If both left and right height are equal then
+the bottom level would be full from left to right and total no. of nodes in that subtree
+is 2^h - 1. 
+
+If left and right height are not equal then add +1 for current root and go to left child 
+and right child. 
+'''
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        
+        leftHeight = self.getLeftHeight(root)
+        rightHeight = self.getRightHeight(root)
+        
+        if leftHeight == rightHeight: 
+            return 2 ** leftHeight - 1
+        else:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)         
+        
+    
+    def getLeftHeight(self, node):
+        height = 0
+        while node:
+            height += 1
+            node = node.left
+        return height
+    
+    def getRightHeight(self, node):
+        height = 0
+        while node:
+            height += 1
+            node = node.right
+        return height
+    
+# Height = H = log(n) where n = total number of nodes
+# Time: O(H * H) = O(log(n)^2) = O(Log^2 n)
+# Auxiliary Space: O(H) = O(log(n))
+    
+ --------------------------------------------------------------------------------------------------------------------
+
+
+
  class Solution:
         # @param {TreeNode} root
         # @return {integer}
