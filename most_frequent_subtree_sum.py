67a679229ba9df72155eaa469cc94db65cd130b7
Erjan <erjan@users.noreply.github.com>
2022-10-01 20:35:19 +0500

    Create most_frequent_subtree_sum.py

---
+++ most_frequent_subtree_sum.py
@@ -0,0 +1,25 @@
+'''
+Given the root of a binary tree, return the most frequent subtree sum. If there is a tie, return all the values with the highest frequency in any order.
+
+The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself).
+'''
+
+
+from collections import defaultdict
+
+class Solution:
+    def findFrequentTreeSum(self, root: Optional[TreeNode]) -> List[int]:
+
+        def subtree_freqs(node, curr_sum=0, freqs=defaultdict(int)):
+                    if node is None:  # this is our base recursive case
+                        return 0, freqs
+                    curr_sum += subtree_freqs(node.left, curr_sum, freqs)[0] + \
+                    subtree_freqs(node.right, curr_sum, freqs)[0]# because we are returning our values in a tuple, we use [0] to specify we want curr_sum
+                    curr_sum += node.val
+                    freqs[curr_sum] += 1
+                    return curr_sum, freqs
+
+        final_freqs = subtree_freqs(root)[1]
+        max_val = max(final_freqs.values())
+        maxis = [k for k, v in final_freqs.items() if v == max_val]
+        return maxis
