d166d4e00821d385e4fe69da0549d76fbed58167
Erjan <erjan@users.noreply.github.com>
2022-10-03 21:36:16 +0500

    Create number_of_provinces.py

---
+++ number_of_provinces.py
@@ -0,0 +1,41 @@
+'''
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+'''
+
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        
+        graph = collections.defaultdict(list)
+        
+        if not M:
+            return 0
+        
+        n = len(M)
+        for i in range(n):
+            for j in range(i+1,n):
+                if M[i][j]==1:
+                    graph[i].append(j)
+                    graph[j].append(i)
+        
+        visit = [False]*n
+        
+        def dfs(u):
+            for v in graph[u]:
+                if visit[v] == False:
+                    visit[v] = True
+                    dfs(v)
+        
+        count = 0
+        for i in range(n):
+            if visit[i] == False:
+                count += 1
+                visit[i] = True
+                dfs(i)
+        
+        return count
