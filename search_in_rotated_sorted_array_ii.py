b620f71d439ce3c7b898dd8ba49cea39e6631b0d
Erjan <erjan@users.noreply.github.com>
2022-10-05 21:41:28 +0500

    Create search_in_rotated_sorted_array_ii.py

---
+++ search_in_rotated_sorted_array_ii.py
@@ -0,0 +1,52 @@
+'''
+There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+You must decrease the overall operation steps as much as possible.
+'''
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        nums.sort()
+        l = 0
+        r = len(nums)-1
+        mid_index = 0
+        
+        while(l<=r):
+            mid_index = (l+r)//2
+            mid_number = nums[mid_index]
+            
+            if mid_number == target:
+                return True
+            if mid_number<target:
+                l = mid_index+1
+            else:
+                r = mid_index-1
+        return False
+----------------------------------------------------------------------------
+def search(self, nums, target):
+    l, r = 0, len(nums)-1
+    while l <= r:
+        mid = l + (r-l)//2
+        if nums[mid] == target:
+            return True
+        while l < mid and nums[l] == nums[mid]: # tricky part
+            l += 1
+        # the first half is ordered
+        if nums[l] <= nums[mid]:
+            # target is in the first half
+            if nums[l] <= target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        # the second half is ordered
+        else:
+            # target is in the second half
+            if nums[mid] < target <= nums[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
+    return False
