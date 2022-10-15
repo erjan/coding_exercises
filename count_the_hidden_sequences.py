c1107813b752c680331ee13bebaad533b1e96e32
Erjan <erjan@users.noreply.github.com>
2022-10-16 00:25:10 +0500

    Create count_the_hidden_sequences.py

---
+++ count_the_hidden_sequences.py
@@ -0,0 +1,12 @@
+    def numberOfArrays(self, diff, lower, upper):
+        A = list(accumulate(diff, initial = 0))
+        return max(0, (upper - lower) - (max(A) - min(A)) + 1)
+      
+-------------------------------------------------------------------------------------------------
+
+class Solution:
+    def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        l = [0]
+        for i in differences:
+            l.append(l[-1]+i)
+        return max(0,(upper-lower+1)-(max(l)-min(l)))
