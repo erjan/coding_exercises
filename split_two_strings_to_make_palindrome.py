7bf7dfadc56e28ce56e9afca1aa574dda296aed7
Erjan <erjan@users.noreply.github.com>
2022-10-18 04:57:28 +0500

    Update split_two_strings_to_make_palindrome.py

--- split_two_strings_to_make_palindrome.py
+++ split_two_strings_to_make_palindrome.py
@@ -9,7 +9,21 @@ Notice that x + y denotes the concatenation of strings x and y.
 '''
 
 
-
+def checkPalindromeFormation(self, a: str, b: str) -> bool:
+        def check(a,b,n):
+            i, j = 0, n - 1
+            while(i<n):
+                if(a[i]!=b[j]):
+                      break 
+                i+=1
+                j-=1
+            xa = a[i:j+1]
+            xb = b[i:j+1]
+            if(xa==xa[::-1] or xb==xb[::-1]):
+                return True 
+        return check(a,b,len(a)) or check(b,a,len(a))
+    
+--------------------------------------------------------------------------------------------------------    
 class Solution:
     def checkPalindromeFormation(self, a: str, b: str) -> bool:
         n=len(a)
