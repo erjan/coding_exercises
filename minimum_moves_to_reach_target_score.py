fdc4a8b47fe11c9fbba97727f91c04c34f952b01
Erjan <erjan@users.noreply.github.com>
2022-10-19 01:11:55 +0500

    Update minimum_moves_to_reach_target_score.py

--- minimum_moves_to_reach_target_score.py
+++ minimum_moves_to_reach_target_score.py
@@ -24,19 +24,21 @@ class Solution:
        return moves
       
 --------------------------------------------------------------------------------------------
-def minMoves(self, target: int, maxDoubles: int) -> int:
-
-    ans=0
-    while target!=1:
-        if target%2==0 and maxDoubles!=0: 
-            target=target/2
-            maxDoubles-=1
-            ans+=1                   
-        else:
-            if maxDoubles==0:
-                ans=ans+(target-1)
-                target=1 
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:                
+        moves = 0        
+        while target != 1:
+            
+            if target %2 == 0 and maxDoubles!= 0:
+                target =target//2
+                maxDoubles -=1
+                moves+=1                
             else:
-                target-=1
-                ans+=1
-    return int(ans)
+                if maxDoubles == 0:
+                    moves += target-1
+                    target = 1
+                else:
+                    target -=1
+                    moves += 1
+        return moves
+                
