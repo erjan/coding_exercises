347c08971837d1a9574db2e29449ccc29f6e6056
Erjan <erjan@users.noreply.github.com>
2022-10-21 05:59:01 +0500

    Update maximum_length_of_pair_chain.py

--- maximum_length_of_pair_chain.py
+++ maximum_length_of_pair_chain.py
@@ -32,3 +32,31 @@ class Solution:
                 if pairs[i][0]>pairs[j][1]:
                     dp[i]=max(dp[i],dp[j]+1)
         return dp[len(pairs)-1]
+
+    
+-----------------------------------------------------------------------------------
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pairs.sort()
+        length = len(pairs)
+        prev = None
+        count = 0
+        for i in range(0, len(pairs)):
+            #Initial Case
+            if not prev:
+                count += 1
+                prev = pairs[i]
+            #Normal case explained in the question description
+            elif prev[-1] < pairs[i][0]:
+                prev = pairs[i]
+                count += 1
+            #if the current pair has smaller end than the previous then we take this
+            #how do we know this is valid?
+            #if we had something like this -> [[1,2],[3,10],[4,5],[6,10]]
+            #we greedily replace 3,10 with 4,5 as it has smaller end which can let us
+            #find more pairs, how do we know [4,5] makes valid chain with pairs before [3,10]
+            #because we know that 4 > 3 and the reason why [3,10] formed pairs is that 3 was bigger
+            #than numbers before it for eg: [3,10] and [1,2] --> 3 > 2 and so 4 is also bigger than 2
+            elif prev[-1] > pairs[i][-1]:
+                prev = pairs[i]
+        return count
