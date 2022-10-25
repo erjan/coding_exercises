971cbfd891a78976884ce4a185bacd8a5e7a4f5e
Erjan <erjan@users.noreply.github.com>
2022-10-25 10:24:23 +0500

    Create check_if_number_is_a_sum_of_powers_of_three.py

---
+++ check_if_number_is_a_sum_of_powers_of_three.py
@@ -0,0 +1,27 @@
+'''
+Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
+
+An integer y is a power of three if there exists an integer x such that y == 3x.
+'''
+
+class Solution:
+    
+    def checkPowersOfThree(self, n: int) -> bool:
+                
+        def ternary(n):
+            e = n//3
+            q = n%3
+            if n == 0:
+                return '0'
+            elif e == 0:
+                return str(q)
+            else:
+                return ternary(e) + str(q)
+        
+        
+        s = ternary(n)
+        
+        if '2' in s:
+            return False
+        return True
+--------------------------------------------------------------------------------------------------------------------------------------------
