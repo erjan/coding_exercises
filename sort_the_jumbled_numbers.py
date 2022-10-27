6cf0c0bda28eade0303a1467e42b97685c7b96a5
Erjan <erjan@users.noreply.github.com>
2022-10-27 20:44:26 +0500

    Update sort_the_jumbled_numbers.py

--- sort_the_jumbled_numbers.py
+++ sort_the_jumbled_numbers.py
@@ -11,3 +11,35 @@ Elements with the same mapped values should appear in the same relative order as
 The elements of nums should only be sorted based on their mapped values and not be replaced by them.
 '''
 
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        def tval(val):
+            val = str(val)
+            return int("".join([ str(mapping[int(val[i])]) for i in range(len(val))]))
+        nums.sort(key = lambda e:tval(e))
+        return nums
+      
+------------------------------------------------------------------------------------------------------------
+class Solution:
+	def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+		res = []
+		for num in nums:
+			ans = ""
+			for char in str(num):
+				ans += str(mapping[int(char)])
+			res.append(int(ans))
+		final = list(zip(nums, res))
+		final = sorted(final, key=lambda x: x[1])
+		return [tup[0] for tup in final]
+  
+------------------------------------------------------------------------------------------------------------------------------
+
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        m = { str(i): str(v) for i, v in enumerate(mapping) }
+
+        def mapFunc(n):
+            return int(''.join(m[i] for i in str(n)))
+                
+        return sorted(nums, key=mapFunc)
+
