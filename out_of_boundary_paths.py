8c6ddba80528ecd6c42a39573ab6f95252c1cb75
Erjan <erjan@users.noreply.github.com>
2022-10-28 12:27:38 +0500

    Update out_of_boundary_paths.py

--- out_of_boundary_paths.py
+++ out_of_boundary_paths.py
@@ -4,22 +4,32 @@ There is an m x n grid with a ball. The ball is initially at the position [start
 Given the five integers m, n, maxMove, startRow, startColumn, return the number of paths to move the ball out of the grid boundary. Since the answer can be very large, return it modulo 109 + 7.
 '''
 
-
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-
+        
+		# define the dp array
+        dp = [[[-1]*(maxMove+1) for _ in range(n+1)] for _ in range(m+1)]
+        
         def solve(i, j, maxMove):
             if maxMove < 0:
                 return 0
             if i < 0 or i >= m or j < 0 or j >= n:
                 return 1
             
+			# if the dp array at this position contains some value(not -1) then it means it has been computed earlier
+			# so we don't need to compute again, hence return whatever value is present in dp.
+            if dp[i][j][maxMove] != -1:
+                return dp[i][j][maxMove]
+            
+			# otherwise compute the value
             a = solve(i-1, j, maxMove - 1)
             b = solve(i+1, j, maxMove - 1)
             c = solve(i, j-1, maxMove - 1)
             d = solve(i, j+1, maxMove - 1)
             
-            return a + b + c + d
+			# store the computed value in dp array so that we do not need to compute it again when the same input occurs again.
+            dp[i][j][maxMove] = a + b + c + d
+            return dp[i][j][maxMove]
         
         return solve(startRow, startColumn, maxMove) % 1000000007
 ---------------------------------------------------------------------------------------------------------------------------
