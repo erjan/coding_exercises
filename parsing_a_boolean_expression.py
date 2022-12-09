3c46aea2649bb82ebdb957b4589f7049d3550463
Erjan <erjan@users.noreply.github.com>
2022-12-09 14:05:40 +0500

    Update parsing_a_boolean_expression.py

--- parsing_a_boolean_expression.py
+++ parsing_a_boolean_expression.py
@@ -11,6 +11,31 @@ Given a string expression that represents a boolean expression, return the evalu
 It is guaranteed that the given expression is valid and follows the given rules.
 '''
 
+
+class Solution:
+    operands = {"!", "&", "|", "t", "f"}
+    values = {"t", "f"}
+
+    def parseBoolExpr(self, expression: str) -> bool:
+        stack = []
+        for c in expression:
+            if c == ")":
+                val = stack.pop()
+                args = set()
+                while val in Solution.values:
+                    args.add(val)
+                    val = stack.pop()
+                if val == "!":
+                    stack.append("f" if "t" in args else "t")
+                elif val == "&":
+                    stack.append("f" if "f" in args else "t")
+                elif val == "|":
+                    stack.append("t" if "t" in args else "f")
+            elif c in Solution.operands:
+                stack.append(c)
+        return stack[0] == "t"
+
+--------------------------------------------------------------------------------------------------------------------------------------
 import re
 
 class Solution:
