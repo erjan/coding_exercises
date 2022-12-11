475c0204b9667701630dc8a81e5e152cbc86f6a8
Erjan <erjan@users.noreply.github.com>
2022-12-11 21:15:15 +0500

    Update maximum_deletions_on_a_string.py

--- maximum_deletions_on_a_string.py
+++ maximum_deletions_on_a_string.py
@@ -8,24 +8,21 @@ For example, if s = "ababc", then in one operation, you could delete the first t
 Return the maximum number of operations needed to delete all of s.
 '''
 
-class Solution:
     def deleteString(self, s: str) -> int:
-        if min(s) == max(s):
-            return len(s)
         n = len(s)
-        eq = [[0]*(n+1) for i in range(n+1)]
-        for i in range(n-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                if s[i] == s[j]:
-                    eq[i][j] = 1 + eq[i+1][j+1]
+        if n == 1: return 1
+		if (len(set(s)) == 1): return n
         
-        dp = [0]*n
-        for i in range(n-1, -1, -1):
-            dp[i] = 1
-            j = 1
-            while i+2*j <= n:
-                if eq[i][i+j] >= j:
-                    dp[i] = max(dp[i], dp[i+j]+1)
-                j += 1
-
-        return dp[0]
+        @cache
+        def dp(i):
+            if i == n - 1: return 1
+            res = 0
+            l = (n - i) // 2
+            temp = ""
+            for j in range(1, l + 1):
+                temp += s[i + j - 1]
+                if temp == s[i+j:i+2*j]:
+                    res = max(res, dp(i+j))
+            return res + 1        
+        
+        return dp(0)
