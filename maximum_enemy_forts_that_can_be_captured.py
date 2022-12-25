a4a2e1b860e34c01933644bbd290e7c9a0ce2d1d
Erjan <erjan@users.noreply.github.com>
2022-12-25 15:31:28 +0500

    Create maximum_enemy_forts_that_can_be_captured.py

---
+++ maximum_enemy_forts_that_can_be_captured.py
@@ -0,0 +1,25 @@
+'''
+You are given a 0-indexed integer array forts of length n representing the positions of several forts. forts[i] can be -1, 0, or 1 where:
+
+-1 represents there is no fort at the ith position.
+0 indicates there is an enemy fort at the ith position.
+1 indicates the fort at the ith the position is under your command.
+Now you have decided to move your army from one of your forts at position i to an empty position j such that:
+
+0 <= i, j <= n - 1
+The army travels over enemy forts only. Formally, for all k where min(i,j) < k < max(i,j), forts[k] == 0.
+While moving the army, all the enemy forts that come in the way are captured.
+
+Return the maximum number of enemy forts that can be captured. In case it is impossible to move your army, or you do not have any fort under your command, return 0.
+'''
+
+
+
+class Solution: 
+    def captureForts(self, forts: List[int]) -> int:
+        ans = ii = 0 
+        for i, x in enumerate(forts): 
+            if x: 
+                if forts[ii] == -x: ans = max(ans, i-ii-1)
+                ii = i 
+        return ans 
