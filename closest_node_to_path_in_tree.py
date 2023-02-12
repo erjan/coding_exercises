b702b9e5f5aaa35afe11e76324665838646937e0
Erjan <erjan@users.noreply.github.com>
2023-02-12 23:56:20 +0500

    Update closest_node_to_path_in_tree.py

--- closest_node_to_path_in_tree.py
+++ closest_node_to_path_in_tree.py
@@ -25,3 +25,56 @@ Return an integer array answer of length m, where answer[i] is the answer to the
                             tmp.append(b)
                 que = tmp
         return [min(range(n), key=lambda x: D[x][a]+D[x][b]+D[x][q]) for a,b,q in query]
+-------------------------------------------------------------------------------------------------
+class Solution:
+    def closestNode(self, n, edges, query):
+        dict1 = collections.defaultdict(list)
+
+        for i,j in edges:
+            dict1[i].append(j)
+            dict1[j].append(i)
+
+        def dfs(start,end):
+            if start == end:
+                return True
+
+            visited.add(start)
+
+            for neighbor in dict1[start]:
+                if neighbor not in visited:
+                    stack.append(neighbor)
+                    if dfs(neighbor,end):
+                        return True
+                    stack.remove(neighbor)
+
+
+        def bfs(ans,path):
+            visited = set()
+
+            visited.add(ans[0])
+
+            while ans:
+                node = ans.pop(0)
+
+                if node in path:
+                    return node
+
+                for neighbor in dict1[node]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        ans.append(neighbor)
+
+        result = []
+
+        for i in query:
+            visited = set()
+            stack = [i[0]]
+            dfs(i[0],i[1])
+
+            if i[2] in stack:
+                result.append(i[2])
+                continue
+
+            result.append(bfs([i[2]],stack))
+
+        return result
