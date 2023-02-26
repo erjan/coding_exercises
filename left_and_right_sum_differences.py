174ae02f21e0ccff6c2f26d9c740e1177c875bb3
Erjan <erjan@users.noreply.github.com>
2023-02-26 11:27:52 +0500

    Create left_and_right_sum_differences.py

---
+++ left_and_right_sum_differences.py
@@ -0,0 +1,24 @@
+'''
+Given a 0-indexed integer array nums, find a 0-indexed integer array answer where:
+
+answer.length == nums.length.
+answer[i] = |leftSum[i] - rightSum[i]|.
+Where:
+
+leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+Return the array answer.
+'''
+
+
+
+class Solution:
+    def leftRigthDifference(self, nums: List[int]) -> List[int]:
+        prefix = 0 
+        suffix = sum(nums)
+        ans = []
+        for x in nums: 
+            prefix += x
+            ans.append(abs(prefix - suffix))
+            suffix -= x
+        return ans 
