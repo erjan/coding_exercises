8f4b9dcbb06a95b30e74a8e48dee66bfbfc5e8c4
Erjan <erjan@users.noreply.github.com>
2023-03-19 12:13:02 +0500

    Update maximize_greatness_of_an_array.py

--- maximize_greatness_of_an_array.py
+++ maximize_greatness_of_an_array.py
@@ -28,4 +28,45 @@ class Solution:
                 ans+=1
 
         return ans
+    
+-----------------------------------------------------------------------------------------
+import heapq 
+class Solution(object):
+    def maximizeGreatness(self, nums):
+        for i in range(len(nums)):
+            nums[i]=-nums[i]
+        heapq.heapify(nums)
+        # print(nums)
+        arr=[]
+        ans=0
+
+        for i in range(len(nums)):
+            curr_min = heapq.heappop(nums)
+            heapq.heappush(arr, curr_min)
+            
+            if arr and nums and -arr[0] > -nums[0]:
+                ans += 1
+                heapq.heappop(arr)
+                
+        # print(ans)
+        return ans
+    
+---------------------------------------------------------------------------------------------------
+class Solution:
+    def maximizeGreatness(self, nums: List[int]) -> int:
+        minHeap = []
+        for num in nums:
+            heapq.heappush(minHeap, num)
+        res = 0
+        nums.sort()
+        for num in nums:
+            while minHeap and minHeap[0] <= num:
+                heapq.heappop(minHeap)
+            if minHeap:
+                heapq.heappop(minHeap)
+                res += 1
+            if not minHeap:
+                break
+        return res
+        
         
