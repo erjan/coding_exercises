2d459d9e754b1f5d5edd810d13670ff36a8fe578
Erjan <erjan@users.noreply.github.com>
2023-04-03 02:07:21 +0500

    Create Convert an Array Into a 2D Array With Conditions.py

---
+++ Convert an Array Into a 2D Array With Conditions.py
@@ -0,0 +1,24 @@
+'''
+Companies
+You are given an integer array nums. You need to create a 2D array from nums satisfying the following conditions:
+
+The 2D array should contain only the elements of the array nums.
+Each row in the 2D array contains distinct integers.
+The number of rows in the 2D array should be minimal.
+Return the resulting array. If there are multiple answers, return any of them.
+
+Note that the 2D array can have a different number of elements on each row.
+'''
+
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        nums.sort(reverse=True)
+        lignes = []
+        for num in nums:
+            for ligne in lignes:
+                if num not in ligne:
+                    ligne.append(num)
+                    break
+            else:
+                lignes.append([num])
+        return lignes
