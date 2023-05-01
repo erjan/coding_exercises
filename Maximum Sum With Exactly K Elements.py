506f0e6a3ad2fd1d540cc175ef5a55165ca9c680
Erjan <erjan@users.noreply.github.com>
2023-05-01 11:53:56 +0500

    Create Maximum Sum With Exactly K Elements.py

---
+++ Maximum Sum With Exactly K Elements.py
@@ -0,0 +1,28 @@
+'''
+You are given a 0-indexed integer array nums and an integer k. Your task is to perform the following operation exactly k times in order to maximize your score:
+
+Select an element m from nums.
+Remove the selected element m from the array.
+Add a new element with a value of m + 1 to the array.
+Increase your score by m.
+Return the maximum score you can achieve after performing the operation exactly k times.
+'''
+
+
+class Solution:
+    def maximizeSum(self, nums: List[int], k: int) -> int:
+
+        nums= [-n for n in nums]
+        heapify(nums)
+        ans = 0
+        
+
+        for i in range(k):
+
+            temp = -heapq.heappop(nums)
+            ans+= temp
+
+            temp +=1
+            heapq.heappush(nums,-temp)
+        
+        return ans
