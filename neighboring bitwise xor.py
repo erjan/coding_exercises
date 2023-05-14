bb68390c4dc72ce68dfa5a8ec74f2a8ef3bef7b3
Erjan <erjan@users.noreply.github.com>
2023-05-14 23:36:13 +0500

    Create neighboring bitwise xor.py

---
+++ neighboring bitwise xor.py
@@ -0,0 +1,28 @@
+'''
+A 0-indexed array derived with length n is derived by computing the bitwise XOR (⊕) of adjacent values in a binary array original of length n.
+
+Specifically, for each index i in the range [0, n - 1]:
+
+If i = n - 1, then derived[i] = original[i] ⊕ original[0].
+Otherwise, derived[i] = original[i] ⊕ original[i + 1].
+Given an array derived, your task is to determine whether there exists a valid binary array original that could have formed derived.
+
+Return true if such an array exists or false otherwise.
+
+A binary array is an array containing only 0's and 1's
+'''
+
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        
+        #we try 0 and 1 as potential first elements
+        for first in [0,1]:
+            original=first
+            for result in derived[:-1]:
+                #we apply the reverse operation as shown in the pictures
+                original=original^result
+            #finally, we check if the validity condtion is true
+            if original^first==derived[-1]:
+                return True
+        return False
+        
