840bdb928eb618bda56babaf5c528a8e464f46af
Erjan <erjan@users.noreply.github.com>
2023-05-14 21:50:13 +0500

    Update Sum in a Matrix.py
    solution using max heap

--- Sum in a Matrix.py
+++ Sum in a Matrix.py
@@ -22,3 +22,22 @@ class Solution:
             score += max(col)
 
         return score
+    
+------------------------------------------------------------------------------------
+#solution using maxheaps
+
+class Solution:
+    def matrixSum(self, nums: List[List[int]]) -> int:
+        num_rows, num_cols = len(nums), len(nums[0])
+        max_heaps = [[] * num_cols for _ in range(num_rows)]
+        for r, row in enumerate(nums):
+            for e in (row):
+                heapq.heappush(max_heaps[r], -e)
+        
+        score = 0
+        for _ in range(num_cols):
+            curr_max = 0
+            for r, max_heap in enumerate(max_heaps):
+                curr_max = max(curr_max, -heapq.heappop(max_heap))
+            score += curr_max
+        return score
