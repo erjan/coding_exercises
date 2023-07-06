b9d3810fc76e790286b33820561594018b4dc1af
Erjan <erjan@users.noreply.github.com>
2023-07-07 02:01:27 +0500

    Create Convert a Number to Hexadecimal.py

---
+++ Convert a Number to Hexadecimal.py
@@ -0,0 +1,28 @@
+'''
+Given an integer num, return a string representing its hexadecimal representation. For negative integers, two’s complement method is used.
+
+All the letters in the answer string should be lowercase characters, and there should not be any leading zeros in the answer except for the zero itself.
+
+Note: You are not allowed to use any built-in library method to directly solve this problem.
+'''
+
+
+class Solution:
+    def toHex(self, num: int) -> str:
+        
+        if num == 0:
+            return '0'
+        if num < 0:
+            num = (1<<32) + num
+
+        m = '0123456789abcdef'
+
+        res = ''
+
+        while num:
+
+            full,digit = divmod(num,16)            
+            res = m[digit] + res  #this is crucial! res += m[digit] is wrong!
+            num = full
+        return res
+            
