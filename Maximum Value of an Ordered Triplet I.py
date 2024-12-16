ae33dee366d799940d0df04b8ec8da0dd922f9d8
Erjan <erjan@users.noreply.github.com>
2024-12-16 21:32:32 +0500

    Create Maximum Value of an Ordered Triplet I.py

---
+++ Maximum Value of an Ordered Triplet I.py
@@ -0,0 +1,22 @@
+
+'''
+You are given a 0-indexed integer array nums.
+
+Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+
+The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+'''
+
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        res = 0
+
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i+1, n):
+                for k in range(j+1,n):
+                    res = max(res, (nums[i]-nums[j])*nums[k])
+        return res
+
