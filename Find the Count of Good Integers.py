871b506d8fd59f24801e7f4effe4684a1e6bad8e
Erjan <erjan@users.noreply.github.com>
2025-04-12 17:19:06 +0500

    Create Find the Count of Good Integers.py

---
+++ Find the Count of Good Integers.py
@@ -0,0 +1,43 @@
+'''
+You are given two positive integers n and k.
+
+An integer x is called k-palindromic if:
+
+x is a palindrome.
+x is divisible by k.
+An integer is called good if its digits can be rearranged to form a k-palindromic integer. For example, for k = 2, 2020 can be rearranged to form the k-palindromic integer 2002, whereas 1010 cannot be rearranged to form a k-palindromic integer.
+
+Return the count of good integers containing n digits.
+
+Note that any integer must not have leading zeros, neither before nor after rearrangement. For example, 1010 cannot be rearranged to form 101.
+'''
+
+
+class Solution:
+    def countGoodIntegers(self, n: int, k: int) -> int:
+        dictionary = set()
+        base = 10 ** ((n - 1) // 2)
+        skip = n & 1
+        # Enumerate the number of palindrome numbers of n digits
+        for i in range(base, base * 10):
+            s = str(i)
+            s += s[::-1][skip:]
+            palindromicInteger = int(s)
+            # If the current palindrome number is a k-palindromic integer
+            if palindromicInteger % k == 0:
+                sorted_s = "".join(sorted(s))
+                dictionary.add(sorted_s)
+
+        fac = [factorial(i) for i in range(n + 1)]
+        ans = 0
+        for s in dictionary:
+            cnt = [0] * 10
+            for c in s:
+                cnt[int(c)] += 1
+            # Calculate permutations and combinations
+            tot = (n - cnt[0]) * fac[n - 1]
+            for x in cnt:
+                tot //= fac[x]
+            ans += tot
+
+        return ans
