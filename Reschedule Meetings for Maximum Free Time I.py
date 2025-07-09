62e54f398c6956c51c90fb9cc134ee0d4c73abe8
Erjan <erjan@users.noreply.github.com>
2025-07-09 08:51:46 +0500

    Create Reschedule Meetings for Maximum Free Time I.py

---
+++ Reschedule Meetings for Maximum Free Time I.py
@@ -0,0 +1,30 @@
+'''
+You are given an integer eventTime denoting the duration of an event, where the event occurs from time t = 0 to time t = eventTime.
+
+You are also given two integer arrays startTime and endTime, each of length n. These represent the start and end time of n non-overlapping meetings, where the ith meeting occurs during the time [startTime[i], endTime[i]].
+
+You can reschedule at most k meetings by moving their start time while maintaining the same duration, to maximize the longest continuous period of free time during the event.
+
+The relative order of all the meetings should stay the same and they should remain non-overlapping.
+
+Return the maximum amount of free time possible after rearranging the meetings.
+
+Note that the meetings can not be rescheduled to a time outside the event.
+'''
+
+class Solution:
+    def maxFreeTime(self, eventTime: int, k: int, startTime: list[int], endTime: list[int]) -> int:
+        n = len(startTime)
+        res = 0
+        sum_durations = [0] * (n + 1)
+
+        for i in range(n):
+            sum_durations[i + 1] = sum_durations[i] + endTime[i] - startTime[i]
+
+        for i in range(k - 1, n):
+            right = eventTime if i == n - 1 else startTime[i + 1]
+            left = 0 if i == k - 1 else endTime[i - k]
+            free_time = right - left - (sum_durations[i + 1] - sum_durations[i - k + 1])
+            res = max(res, free_time)
+
+        return res
